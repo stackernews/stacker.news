57e96ac02b7b0b37af59473f330e9d90c3e307b8
keyan <keyan.kousha+huumn@gmail.com>
2021-05-20 12:21:11 -0500

    retry on serialization errors

--- api/resolvers/serial.js
+++ api/resolvers/serial.js
@@ -1,22 +1,32 @@
 const { UserInputError } = require('apollo-server-micro')
+const retry = require('async-retry')
 
 async function serialize (models, call) {
-  try {
-    const [, result] = await models.$transaction([
-      models.$executeRaw(SERIALIZE),
-      call
-    ])
-    return result
-  } catch (error) {
-    console.log(error)
-    if (error.message.includes('SN_INSUFFICIENT_FUNDS')) {
-      throw new UserInputError('insufficient funds')
+  await retry(async bail => {
+    try {
+      const [, result] = await models.$transaction([
+        models.$executeRaw(SERIALIZE),
+        call
+      ])
+      return result
+    } catch (error) {
+      console.log(error)
+      if (error.message.includes('SN_INSUFFICIENT_FUNDS')) {
+        bail(new UserInputError('insufficient funds'))
+      }
+      if (error.message.includes('SN_NOT_SERIALIZABLE')) {
+        bail(new Error('wallet balance transaction is not serializable'))
+      }
+      if (error.message.includes('40001')) {
+        throw new Error('wallet balance serialization failure - retry again')
+      }
+      bail(error)
     }
-    if (error.message.includes('SN_NOT_SERIALIZABLE')) {
-      throw new Error('wallet transaction isolation level is not serializable')
-    }
-    throw error
-  }
+  }, {
+    minTimeout: 100,
+    factor: 1.1,
+    retries: 5
+  })
 }
 
 const SERIALIZE = 'SET TRANSACTION ISOLATION LEVEL SERIALIZABLE'

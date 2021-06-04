631da6c43931765f6bcd0f53b23d6ac9c4aa6db9
keyan <keyan.kousha+huumn@gmail.com>
2021-06-04 11:49:15 -0400

    force build because node platform doesn't respect buildfile

--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
+echo installing yarn
+
 # install yarn
 sudo npm install yarn -g
 
+echo link
+
 # make sure it's in path
 sudo ln -s "$(npm bin --global)"/yarn /usr/bin/yarn
+
+echo try to build
 
 # build it
 sudo yarn build

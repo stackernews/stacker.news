876ac52a9506fa5cdf2270f835de7c5d26cd3f6c
keyan <keyan.kousha+huumn@gmail.com>
2021-06-09 16:29:15 -0500

    try not globally storing lnd and create fresh on include

--- api/lnd/index.js
+++ api/lnd/index.js
@@ -1,12 +1,9 @@
 import lndService from 'ln-service'
 
-if (!global.lnd) {
-  const { lnd } = lndService.authenticatedLndGrpc({
-    cert: process.env.LND_CERT,
-    macaroon: process.env.LND_MACAROON,
-    socket: process.env.LND_SOCKET
-  })
-  global.lnd = lnd
-}
+const { lnd } = lndService.authenticatedLndGrpc({
+  cert: process.env.LND_CERT,
+  macaroon: process.env.LND_MACAROON,
+  socket: process.env.LND_SOCKET
+})
 
-export default global.lnd
+export default lnd

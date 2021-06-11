80c5cff3cf3fcb556fea0b38a3e3ac513ad15e5c
keyan <keyan.kousha+huumn@gmail.com>
2021-06-10 19:49:20 -0500

    remove lndcert ... using unsafe tls instead

--- api/lnd/index.js
+++ api/lnd/index.js
@@ -1,7 +1,7 @@
 import lndService from 'ln-service'
 
 const { lnd } = lndService.authenticatedLndGrpc({
-  // cert: process.env.LND_CERT,
+  cert: process.env.LND_CERT,
   macaroon: process.env.LND_MACAROON,
   socket: process.env.LND_SOCKET
 })

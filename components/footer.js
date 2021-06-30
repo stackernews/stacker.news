0ddd4ca2e05cf18f5c1469484072dedc2aa55d9d
keyan <keyan.kousha+huumn@gmail.com>
2021-06-30 18:01:28 -0500

    foss and twitter link in footer

--- components/footer.js
+++ components/footer.js
@@ -3,6 +3,9 @@ import gql from 'graphql-tag'
 import { Container } from 'react-bootstrap'
 import { CopyInput } from './form'
 import styles from './footer.module.css'
+import Texas from '../svgs/texas.svg'
+import Github from '../svgs/github-fill.svg'
+import Twitter from '../svgs/twitter-fill.svg'
 
 export default function Footer () {
   const query = gql`
@@ -15,10 +18,10 @@ export default function Footer () {
 
   return (
     <footer>
-      {data &&
-        <Container>
+      <Container>
+        {data &&
           <div
-            className={`text-small my-3 mx-auto ${styles.connect}`}
+            className={`text-small mx-auto mb-2 ${styles.connect}`}
           >
             <span className='nav-item text-muted mr-2'>connect:</span>
             <CopyInput
@@ -27,8 +30,17 @@ export default function Footer () {
               readOnly
               placeholder={data.connectAddress}
             />
-          </div>
-        </Container>}
+          </div>}
+        <small>
+          <a className='text-dark d-inline-flex' href='https://github.com/stackernews/stacker.news'>
+            This is free open source software <Github width={20} height={20} className='mx-1' />
+          </a>
+          <span className='d-inline-flex text-muted'>
+            made with sound love in Austin <Texas className='mx-1' width={20} height={20} />
+            by <a href='https://twitter.com/k00bideh' className='text-twitter d-inline-flex'><Twitter width={20} height={20} className='ml-1' />@k00bideh</a>
+          </span>
+        </small>
+      </Container>
     </footer>
   )
 }

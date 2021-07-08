eab1c69c7dead49684f555b06c8d13dee1351a99
keyan <keyan.kousha+huumn@gmail.com>
2021-07-08 10:14:31 -0500

    don't delay capture

--- pages/api/capture/[[...path]].js
+++ pages/api/capture/[[...path]].js
@@ -4,7 +4,7 @@ import path from 'path'
 export default async function handler (req, res) {
   const url = process.env.SELF_URL + '/' + path.join(...(req.query.path || []))
   res.setHeader('Content-Type', 'image/png')
-  const streams = await new Pageres({ crop: true, delay: 1 })
+  const streams = await new Pageres({ crop: true })
     .src(url, ['600x300'])
     .run()
   res.status(200).end(streams[0])

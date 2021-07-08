b31e4e5b057e757e090a53bd593806d251523dc2
keyan <keyan.kousha+huumn@gmail.com>
2021-07-08 13:43:03 -0500

    1 sat action tooltip

---
+++ components/action-tooltip.js
@@ -0,0 +1,17 @@
+import { OverlayTrigger, Tooltip } from 'react-bootstrap'
+
+export default function ActionTooltip ({ children }) {
+  return (
+    <OverlayTrigger
+      placement='bottom'
+      overlay={
+        <Tooltip>
+          1 sat
+        </Tooltip>
+      }
+      trigger={['hover', 'focus', 'active']}
+    >
+      {children}
+    </OverlayTrigger>
+  )
+}

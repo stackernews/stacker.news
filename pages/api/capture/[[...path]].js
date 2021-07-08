248f5a9967253a65524fb3717bc911464065c8d0
keyan <keyan.kousha+huumn@gmail.com>
2021-07-07 19:45:02 -0500

    use env for url

--- pages/api/capture/[[...path]].js
+++ pages/api/capture/[[...path]].js
@@ -2,7 +2,7 @@ import Pageres from 'pageres'
 import path from 'path'
 
 export default async function handler (req, res) {
-  const url = 'http://' + path.join('localhost:3000', ...(req.query.path || []))
+  const url = process.env.SELF_URL + '/' + path.join(...(req.query.path || []))
   res.setHeader('Content-Type', 'image/png')
   const streams = await new Pageres({ crop: true, delay: 1 })
     .src(url, ['600x300'])

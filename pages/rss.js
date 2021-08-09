f92b36699a6660795cb0e0e577bc1fed6a1d8e88
keyan <keyan.kousha+huumn@gmail.com>
2021-08-09 14:47:39 -0500

    fix graphql query for rss

--- pages/rss.js
+++ pages/rss.js
@@ -1,30 +1,23 @@
 
 import ApolloClient from '../api/client'
-import { gql } from '@apollo/client'
 import generateRssFeed from '../lib/rss'
+import { MORE_ITEMS } from '../fragments/items'
 
 export default function RssFeed () {
   return null
 }
 
-export async function getServerSideProps({ req, res }) {
+export async function getServerSideProps ({ req, res }) {
   const emptyProps = { props: {} } // to avoid server side warnings
-  const { error, data } = await (await ApolloClient(req)).query({
-    query: gql`
-      query Items {
-        items {
-          createdAt
-          id
-          title
-        }
-      }
-    `,
+  const { error, data: { moreItems: { items } } } = await (await ApolloClient(req)).query({
+    query: MORE_ITEMS,
+    variables: { sort: 'hot' }
   })
 
-  if (!data.items || error) return emptyProps
+  if (!items || error) return emptyProps
 
-  res.setHeader("Content-Type", "text/xml")
-  res.write(generateRssFeed(data.items))
+  res.setHeader('Content-Type', 'text/xml')
+  res.write(generateRssFeed(items))
   res.end()
 
   return emptyProps

e288f88ec78064b4a49a7e2d59ee2dee5aa245e4
keyan <keyan.kousha+huumn@gmail.com>
2021-10-07 14:03:54 -0700

    share metadata functions

--- pages/api/lnurlp/[username]/index.js
+++ pages/api/lnurlp/[username]/index.js
@@ -1,4 +1,5 @@
 import models from '../../../../api/models'
+import { lnurlPayMetadataString } from '../../../../lib/lnurl'
 
 export default async ({ query: { username } }, res) => {
   const user = await models.user.findUnique({ where: { name: username } })
@@ -10,10 +11,7 @@ export default async ({ query: { username } }, res) => {
     callback: `https://stacker.news/api/lnurlp/${username}/pay`, // The URL from LN SERVICE which will accept the pay request parameters
     minSendable: 1000, // Min amount LN SERVICE is willing to receive, can not be less than 1 or more than `maxSendable`
     maxSendable: Number.MAX_SAFE_INTEGER,
-    metadata: [[
-      'text/plain', // must always be present
-      `Funding @${username} on stacker.news` // actual metadata content
-    ]], // Metadata json which must be presented as raw string here, this is required to pass signature verification at a later step
+    metadata: lnurlPayMetadataString(username), // Metadata json which must be presented as raw string here, this is required to pass signature verification at a later step
     tag: 'payRequest' // Type of LNURL
   })
 }

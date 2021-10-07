48ff38dc56e46c92ea0bfe425fb3c27a8b1d7f61
keyan <keyan.kousha+huumn@gmail.com>
2021-10-07 12:12:49 -0700

    add max to lnaddr

--- pages/api/lnurlp/[username]/index.js
+++ pages/api/lnurlp/[username]/index.js
@@ -9,6 +9,7 @@ export default async ({ query: { username } }, res) => {
   return res.status(200).json({
     callback: `https://stacker.news/api/lnurlp/${username}/pay`, // The URL from LN SERVICE which will accept the pay request parameters
     minSendable: 1000, // Min amount LN SERVICE is willing to receive, can not be less than 1 or more than `maxSendable`
+    maxSendable: Number.MAX_SAFE_INTEGER,
     metadata: [[
       'text/plain', // must always be present
       `Funding @${username} on stacker.news` // actual metadata content

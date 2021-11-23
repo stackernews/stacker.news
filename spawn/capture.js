48509ac8194d7ed52f4e2682949bb0b95c4227b3
keyan <keyan.kousha+huumn@gmail.com>
2021-11-23 12:35:10 -0600

    add shorter timeout

--- spawn/capture.js
+++ spawn/capture.js
@@ -3,7 +3,7 @@
 const Pageres = require('pageres')
 
 async function captureUrl () {
-  const streams = await new Pageres({ crop: true })
+  const streams = await new Pageres({ crop: true, timeout: 5 })
     .src(process.argv[2], ['600x314'])
     .run()
   process.stdout.write(streams[0], () => process.exit(0))

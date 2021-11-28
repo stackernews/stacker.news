46162f4162feddfa502e89d148fb9b05cf1e4f96
keyan <keyan.kousha+huumn@gmail.com>
2021-11-28 11:37:18 -0600

    abbr wallet at 10k

--- components/header.js
+++ components/header.js
@@ -13,8 +13,8 @@ import { useEffect, useState } from 'react'
 import { randInRange } from '../lib/rand'
 
 const formatSats = n => {
-  if (n < 1e3) return n
-  if (n >= 1e3 && n < 1e6) return +(n / 1e3).toFixed(1) + 'k'
+  if (n < 1e4) return n
+  if (n >= 1e4 && n < 1e6) return +(n / 1e3).toFixed(1) + 'k'
   if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(1) + 'm'
   if (n >= 1e9 && n < 1e12) return +(n / 1e9).toFixed(1) + 'b'
   if (n >= 1e12) return +(n / 1e12).toFixed(1) + 't'

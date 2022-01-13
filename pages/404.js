cefd6793f8faf7eccffa5c926c925b7df5ade102
keyan <keyan.kousha+huumn@gmail.com>
2022-01-13 12:16:00 -0600

    different 400/500 image component

--- pages/404.js
+++ pages/404.js
@@ -1,11 +1,11 @@
-import Image from 'next/image'
+import { Image } from 'react-bootstrap'
 import LayoutCenter from '../components/layout-center'
 import styles from '../styles/404.module.css'
 
 export default function fourZeroFour () {
   return (
     <LayoutCenter>
-      <Image width='500' height='376' src='/maze.gif' />
+      <Image width='500' height='376' src='/maze.gif' fluid />
       <h1 className={styles.fourZeroFour}><span>404</span><span className={styles.notFound}>page not found</span></h1>
     </LayoutCenter>
   )
--- pages/500.js
+++ pages/500.js
@@ -1,11 +1,11 @@
-import Image from 'next/image'
+import { Image } from 'react-bootstrap'
 import LayoutCenter from '../components/layout-center'
 import styles from '../styles/404.module.css'
 
 export default function fourZeroFour () {
   return (
     <LayoutCenter>
-      <Image width='500' height='375' src='/falling.gif' />
+      <Image width='500' height='375' src='/falling.gif' fluid />
       <h1 className={styles.fourZeroFour}><span>500</span><span className={styles.notFound}>server error</span></h1>
     </LayoutCenter>
   )

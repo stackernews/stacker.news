8829fccdeff453e4e57a21ba5c1d6674c6611ec5
keyan <keyan.kousha+huumn@gmail.com>
2022-02-02 15:50:12 -0600

    add top spenders to top page

--- components/top-header.js
+++ components/top-header.js
@@ -6,20 +6,20 @@ import { useRouter } from 'next/router'
 export default function TopHeader ({ cat }) {
   const router = useRouter()
   const within = router.query.within
+  const userType = router.query.userType || 'stacked'
 
   return (
     <>
       <Navbar className='pt-0'>
         <Nav
           className={`${styles.navbarNav} justify-content-around`}
-          activeKey={cat}
+          activeKey={cat.split('/')[0]}
         >
           <Nav.Item>
             <Link href={`/top/posts/${within}`} passHref>
               <Nav.Link
                 eventKey='posts'
                 className={styles.navLink}
-                onClick={() => localStorage.setItem('topWithin', 'day')}
               >
                 posts
               </Nav.Link>
@@ -30,18 +30,16 @@ export default function TopHeader ({ cat }) {
               <Nav.Link
                 eventKey='comments'
                 className={styles.navLink}
-                onClick={() => localStorage.setItem('topWithin', 'week')}
               >
                 comments
               </Nav.Link>
             </Link>
           </Nav.Item>
           <Nav.Item>
-            <Link href={`/top/users/${within}`} passHref>
+            <Link href={`/top/users/stacked/${within}`} passHref>
               <Nav.Link
                 eventKey='users'
                 className={styles.navLink}
-                onClick={() => localStorage.setItem('topWithin', 'month')}
               >
                 users
               </Nav.Link>
@@ -49,6 +47,34 @@ export default function TopHeader ({ cat }) {
           </Nav.Item>
         </Nav>
       </Navbar>
+      {cat.split('/')[0] === 'users' &&
+        <Navbar className='pt-0'>
+          <Nav
+            className={`${styles.navbarNav} justify-content-around`}
+            activeKey={userType}
+          >
+            <Nav.Item>
+              <Link href={`/top/users/stacked/${within}`} passHref>
+                <Nav.Link
+                  eventKey='stacked'
+                  className={styles.navLink}
+                >
+                  stacked
+                </Nav.Link>
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link href={`/top/users/spent/${within}`} passHref>
+                <Nav.Link
+                  eventKey='spent'
+                  className={styles.navLink}
+                >
+                  spent
+                </Nav.Link>
+              </Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar>}
       <Navbar className='pt-0'>
         <Nav
           className={styles.navbarNav}
@@ -59,7 +85,6 @@ export default function TopHeader ({ cat }) {
               <Nav.Link
                 eventKey='day'
                 className={styles.navLink}
-                onClick={() => localStorage.setItem('topWithin', 'day')}
               >
                 day
               </Nav.Link>
@@ -70,7 +95,6 @@ export default function TopHeader ({ cat }) {
               <Nav.Link
                 eventKey='week'
                 className={styles.navLink}
-                onClick={() => localStorage.setItem('topWithin', 'week')}
               >
                 week
               </Nav.Link>
@@ -81,7 +105,6 @@ export default function TopHeader ({ cat }) {
               <Nav.Link
                 eventKey='month'
                 className={styles.navLink}
-                onClick={() => localStorage.setItem('topWithin', 'month')}
               >
                 month
               </Nav.Link>
@@ -92,7 +115,6 @@ export default function TopHeader ({ cat }) {
               <Nav.Link
                 eventKey='year'
                 className={styles.navLink}
-                onClick={() => localStorage.setItem('topWithin', 'year')}
               >
                 year
               </Nav.Link>
@@ -103,7 +125,6 @@ export default function TopHeader ({ cat }) {
               <Nav.Link
                 eventKey='forever'
                 className={styles.navLink}
-                onClick={() => localStorage.removeItem('topWithin')}
               >
                 forever
               </Nav.Link>

2dba0b3ffe46202c234d524ff40e4ac996124450
keyan <keyan.kousha+huumn@gmail.com>
2022-04-26 10:48:30 -0500

    invite warning message

--- pages/invites/index.js
+++ pages/invites/index.js
@@ -116,7 +116,12 @@ export default function Invites () {
 
   return (
     <Layout>
-      <h2 className='text-center mt-3'>invite links</h2>
+      <div className='text-center'>
+        <h2 className='mt-3 mb-0'>
+          invite links
+        </h2>
+        <small className='d-block text-muted font-weight-bold mx-5'>send these to people you trust somewhat, e.g. group chats or DMs</small>
+      </div>
       <InviteForm />
       {active.length > 0 && <InviteList name='active' invites={active} />}
       {inactive.length > 0 && <InviteList name='inactive' invites={inactive} />}

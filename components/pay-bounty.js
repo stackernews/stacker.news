3c3a46be782e531b6979c338476aa294a6f4d351
austinkelsay <austinkelsay@yahoo.com>
2022-11-02 16:55:48 -0500

    Tooltip with bounty amount on pay-bounty button

--- components/pay-bounty.js
+++ components/pay-bounty.js
@@ -1,10 +1,20 @@
 import React from "react"
 import styles from "./pay-bounty.module.css"
+import ActionTooltip from './action-tooltip'
+import {useMe} from "./me"
 
-export default function PayBounty({ children, item }) {
+export default function PayBounty({ children, item, bounty }) {
+    const me = useMe()
+
+    console.log("item", item)
+
+    const fwd2me = me && me?.id === item?.fwdUser?.id
+
     return (
         <div className={styles.payContainer}>
-            <div className={styles.pay}>pay-bounty</div>
+            <ActionTooltip notForm disable={item?.mine || fwd2me} overlayText={`${bounty} sats`}>
+                <div className={styles.pay}>pay-bounty</div>
+            </ActionTooltip>
         </div>
     )
 }

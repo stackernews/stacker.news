16c770c845625909db4d0209ffba764efec86ebf
austinkelsay <austinkelsay@yahoo.com>
2023-01-16 13:04:55 -0600

    Adding sats abbreviation to bounty bag icons

--- components/comment.js
+++ components/comment.js
@@ -160,7 +160,7 @@ export default function Comment ({
                   </div>
                 </>}
                 {item.root.bountyPaidTo && item.root.bountyPaidTo == item.id &&
-                  <ActionTooltip notForm overlayText={`${item.root.bounty} sats paid`}>
+                  <ActionTooltip notForm overlayText={`${abbrNum(item.root.bounty)} sats paid`}>
                     <BountyIcon className={`${styles.bountyIcon} ${'fill-success vertical-align-middle'}`} height={16} width={16} />
                   </ActionTooltip>
                 }

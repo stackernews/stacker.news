236dcae13b4439ad6b2fd0c8923e48b7c34b4e4c
keyan <keyan.kousha+huumn@gmail.com>
2023-05-17 17:56:34 -0500

    remove user analytics dos vector

--- api/typeDefs/growth.js
+++ api/typeDefs/growth.js
@@ -1,15 +1,15 @@
 import { gql } from 'apollo-server-micro'
 
-export default gql`
-  extend type Query {
-    registrationGrowth(when: String): [TimeData!]!
-    itemGrowth(when: String): [TimeData!]!
-    spendingGrowth(when: String): [TimeData!]!
-    spenderGrowth(when: String): [TimeData!]!
-    stackingGrowth(when: String): [TimeData!]!
-    stackerGrowth(when: String): [TimeData!]!
-  }
+// extend type Query {
+//   registrationGrowth(when: String): [TimeData!]!
+//   itemGrowth(when: String): [TimeData!]!
+//   spendingGrowth(when: String): [TimeData!]!
+//   spenderGrowth(when: String): [TimeData!]!
+//   stackingGrowth(when: String): [TimeData!]!
+//   stackerGrowth(when: String): [TimeData!]!
+// }
 
+export default gql`
   type NameValue {
     name: String!
     value: Int!

29efc7fe0196ae9bb70e8f083ed843dc18916e46
ekzyis <ek@stacker.news>
2023-05-17 03:09:55 +0200

    Add docs for local LND setup

--- api/lnd/index.js
+++ api/lnd/index.js
@@ -6,4 +6,13 @@ const { lnd } = lndService.authenticatedLndGrpc({
   socket: process.env.LND_SOCKET
 })
 
+// Check LND GRPC connection
+lndService.getWalletInfo({ lnd }, (err, result) => {
+  if (err) {
+    console.error('LND GRPC connection error')
+    return
+  }
+  console.log('LND GRPC connection successful')
+})
+
 export default lnd

8ea9938cb0f0cc78e427f64361880d856e570b00
keyan <keyan.kousha+huumn@gmail.com>
2023-07-25 20:02:43 -0500

    fix broken image text

--- components/text.js
+++ components/text.js
@@ -200,9 +200,9 @@ export function ZoomableImage ({ src, topLevel, ...props }) {
   if (!src) return null
   if (err) {
     return (
-      <span className='d-flex align-items-center text-warning fw-bold pb-1'>
-        <FileMissing width={18} height={18} className='fill-warning me-1' />
-        broken image <small>stacker probably used an unreliable host</small>
+      <span className='d-flex align-items-baseline text-warning-emphasis fw-bold pb-1'>
+        <FileMissing width={18} height={18} className='fill-warning me-1 align-self-center' />
+        broken image <small className='ms-1'>stacker probably used an unreliable host</small>
       </span>
     )
   }

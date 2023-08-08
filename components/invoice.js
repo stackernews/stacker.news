c9c982238fd57645760bd051561277f4c98fc13f
keyan <keyan.kousha+huumn@gmail.com>
2023-08-08 13:05:36 -0500

    enhance ui

--- components/invoice.js
+++ components/invoice.js
@@ -4,22 +4,26 @@ import Qr from './qr'
 export function Invoice ({ invoice }) {
   let variant = 'default'
   let status = 'waiting for you'
+  let webLn = true
   if (invoice.confirmedAt) {
     variant = 'confirmed'
     status = `${invoice.satsReceived} sats deposited`
+    webLn = false
   } else if (invoice.cancelled) {
     variant = 'failed'
     status = 'cancelled'
+    webLn = false
   } else if (invoice.expiresAt <= new Date()) {
     variant = 'failed'
     status = 'expired'
+    webLn = false
   }
 
   const { nostr } = invoice
 
   return (
     <>
-      <Qr webLn value={invoice.bolt11} statusVariant={variant} status={status} />
+      <Qr webLn={webLn} value={invoice.bolt11} statusVariant={variant} status={status} />
       <div className='w-100'>
         {nostr
           ? <AccordianItem

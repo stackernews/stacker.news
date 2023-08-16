675ec2d83ac6a9e7424b8cfa8a9b0e03f59e93cb
Satoshi Nakamoto <WeAreAllSatoshiNakamoto@proton.me>
2023-08-16 11:13:15 -0400

    Update item_act sql function to implement multi-way forwarding

--- prisma/migrations/20230814233157_multiforward/migration.sql
+++ prisma/migrations/20230814233157_multiforward/migration.sql
@@ -191,3 +191,103 @@ BEGIN
     RETURN item;
 END;
 $$;
+
+-- Update item_act to support multi-way forward splits
+CREATE OR REPLACE FUNCTION item_act(item_id INTEGER, user_id INTEGER, act "ItemActType", act_sats INTEGER)
+RETURNS INTEGER
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    user_msats BIGINT;
+    act_msats BIGINT;
+    fee_msats BIGINT;
+    item_act_id INTEGER;
+    referrer_id INTEGER;
+    fwd_entry record; -- for loop iterator variable to iterate across forward recipients
+    fwd_msats BIGINT; -- for loop variable calculating how many msats to give each forward recipient
+    total_fwd_msats BIGINT; -- accumulator to see how many msats have been forwarded for the act
+BEGIN
+    PERFORM ASSERT_SERIALIZED();
+
+    act_msats := act_sats * 1000;
+    SELECT msats, "referrerId" INTO user_msats, referrer_id FROM users WHERE id = user_id;
+    IF act_msats > user_msats THEN
+        RAISE EXCEPTION 'SN_INSUFFICIENT_FUNDS';
+    END IF;
+
+    -- deduct msats from actor
+    UPDATE users SET msats = msats - act_msats WHERE id = user_id;
+
+    IF act = 'VOTE' THEN
+        RAISE EXCEPTION 'SN_UNSUPPORTED';
+    END IF;
+
+    IF act = 'TIP' THEN
+        -- call to influence weightedVotes ... we need to do this before we record the acts because
+        -- the priors acts are taken into account
+        PERFORM weighted_votes_after_tip(item_id, user_id, act_sats);
+
+        -- take 10% and insert as FEE
+        fee_msats := CEIL(act_msats * 0.1);
+        act_msats := act_msats - fee_msats;
+
+        INSERT INTO "ItemAct" (msats, "itemId", "userId", act, created_at, updated_at)
+            VALUES (fee_msats, item_id, user_id, 'FEE', now_utc(), now_utc())
+            RETURNING id INTO item_act_id;
+
+        total_fwd_msats := 0; -- initialize accumulator to 0
+
+        -- add sats to actees' balance and stacked count
+        FOR fwd_entry IN SELECT "userId", "pct" FROM "ItemForward" WHERE "itemId" = item_id
+        LOOP
+            -- fwd_msats represents the sats for this forward recipient from this particular tip action
+            -- use FLOOR just to ensure we don't introduce more msats than we should, give the rest to OP
+            fwd_msats := FLOOR(act_msats * fwd_entry.pct / 100);
+            -- keep track of how many msats have been forwarded, so we can give any remaining to OP
+            total_fwd_msats := fwd_msats + total_fwd_msats;
+            
+            UPDATE users
+            SET msats = msats + fwd_msats, "stackedMsats" = "stackedMsats" + fwd_msats
+            WHERE id = fwd_entry."userId";
+            -- TODO do we want to apply referrals to forwarded sats, too?
+        END LOOP;
+
+        -- Give OP any remaining msats after forwards have been applied
+        UPDATE users
+        SET msats = msats + act_msats - total_fwd_msats, "stackedMsats" = "stackedMsats" + act_msats - total_fwd_msats
+        WHERE id = (SELECT "userId" FROM "Item" WHERE id = item_id)
+        -- TODO how to handle referrals here?
+        RETURNING "referrerId" INTO referrer_id;
+
+        -- leave the rest as a tip
+        INSERT INTO "ItemAct" (msats, "itemId", "userId", act, created_at, updated_at)
+        VALUES (act_msats, item_id, user_id, 'TIP', now_utc(), now_utc());
+
+        -- call to denormalize sats and commentSats
+        PERFORM sats_after_tip(item_id, user_id, act_msats + fee_msats);
+        -- denormalize bounty paid
+        PERFORM bounty_paid_after_act(item_id, user_id);
+    ELSE -- BOOST, POLL, DONT_LIKE_THIS, STREAM
+        -- call to influence if DONT_LIKE_THIS weightedDownVotes
+        IF act = 'DONT_LIKE_THIS' THEN
+            -- make sure they haven't done this before
+            IF EXISTS (SELECT 1 FROM "ItemAct" WHERE "itemId" = item_id AND "userId" = user_id AND "ItemAct".act = 'DONT_LIKE_THIS') THEN
+                RAISE EXCEPTION 'SN_DUPLICATE';
+            END IF;
+
+            PERFORM weighted_downvotes_after_act(item_id, user_id, act_sats);
+        END IF;
+
+        INSERT INTO "ItemAct" (msats, "itemId", "userId", act, created_at, updated_at)
+            VALUES (act_msats, item_id, user_id, act, now_utc(), now_utc())
+            RETURNING id INTO item_act_id;
+    END IF;
+
+    -- they have a referrer and the referrer isn't the one tipping them
+    IF referrer_id IS NOT NULL AND user_id <> referrer_id THEN
+        PERFORM referral_act(referrer_id, item_act_id);
+    END IF;
+
+    RETURN 0;
+END;
+$$;

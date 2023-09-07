75c7edd9872db52699dd191a586186648f21f073
ekzyis <ek@stacker.news>
2023-09-07 14:31:43 +0200

    Use eslint
    We have found a bug which would have been caught by the react-hooks/exhaustive-deps rule:
    
    /home/ekzyis/programming/stacker.news/components/reply.js
       36:40  error  'onSuccess' is defined but never used                         no-unused-vars
       43:6   error  React Hook useEffect has missing dependencies: 'parentId' and 'replyOpen'. Either include them or remove the dependency array. If 'setReply' needs the current value of 'replyOpen', you can also s
    witch to useReducer instead of useState and read 'replyOpen' in the reducer    react-hooks/exhaustive-deps
       97:6   error  React Hook useCallback has missing dependencies: 'parentId' and 'replyOpen'. Either include them or remove the dependency array. If 'setReply' needs the current value of 'replyOpen', you can also
     switch to useReducer instead of useState and read 'replyOpen' in the reducer  react-hooks/exhaustive-deps
      102:6   error  React Hook useEffect has a missing dependency: 'replyOpen'. Either include it or remove the dependency array
                                                                                   react-hooks/exhaustive-deps
    
    As you can see, there are other problems it detects.
    
    Therefore, I propose that we not only run `standard`, but also `eslint` in our CI pipeline.
    
    I will fix all found problems in the next commits. I will ignore rules in the config which are not necessary for us.

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    // React scope no longer necessary with new JSX transform in React v17+
+    'plugin:react/jsx-runtime'
+  ],
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react',
+    'react-hooks'
+  ],
+  rules: {
+    'react/prop-types': 'off',
+    'react-hooks/exhaustive-deps': 'error'
+  }
+}

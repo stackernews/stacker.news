29c730bea25dadbbf12273010073b93e758a7e0a
ekzyis <ek@stacker.news>
2023-09-09 00:35:47 +0200

    Send diagnostics to slack

--- pages/api/log/index.js
+++ pages/api/log/index.js
@@ -1,11 +1,36 @@
 import models from '../../../api/models'
 
+const slackApiUrl = 'https://slack.com/api'
+const botToken = process.env.SLACK_BOT_TOKEN
+const channelId = process.env.SLACK_CHANNEL_ID
+
+const slackPostMessage = ({ id, level, name, message, env, context }) => {
+  let text = `${new Date().toISOString()} | ${id} [${level}] ${name} | ${message}`
+  if (context) {
+    text += `\n${JSON.stringify(context)}`
+  }
+  if (env) {
+    text += `\n${JSON.stringify(env)}`
+  }
+  return fetch(slackApiUrl + '/chat.postMessage', {
+    method: 'post',
+    headers: {
+      Authorization: `Bearer ${botToken}`,
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify({ channel: channelId, text })
+  })
+}
+
 export default async (req, res) => {
   const { level, name, message, env, context } = req.body
   if (!name) return res.status(400).json({ status: 400, message: 'name required' })
   if (!message) return res.status(400).json({ status: 400, message: 'message required' })
 
-  await models.log.create({ data: { level: level.toUpperCase(), name, message, env, context } })
+  const { id } = await models.log.create({ data: { level: level.toUpperCase(), name, message, env, context } })
 
+  if (botToken && channelId) {
+    slackPostMessage({ id, ...req.body })
+  }
   return res.status(200).json({ status: 200, message: 'ok' })
 }

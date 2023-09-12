e68b637ef7aba503fe81115929dfd51a7a2b30ae
rleed <rleed1@pm.me>
2023-09-12 16:09:46 -0400

    remove bash scripts

--- scripts/social/send-all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo `date -u`
-echo ========
-echo "$1"
-echo ----
-./send-twitter.sh "$1"
-./send-nostr.sh "$1"
-echo .
--- scripts/social/send-nostr.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-EVENT="$1"
-
-echo "$ noscl publish ..."
-noscl publish "$EVENT"

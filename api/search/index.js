568a94137ec2fb7e0cec5cec401f83cc84820d8b
rleed <rleed1@pm.me>
2023-09-20 13:21:19 -0400

    add containers for OpenSearch

--- api/search/index.js
+++ api/search/index.js
@@ -1,7 +1,7 @@
 const os = require('@opensearch-project/opensearch')
 
 const options = process.env.NODE_ENV === 'development'
-  ? { node: 'http://localhost:9200' }
+  ? { node: 'http://opensearch:9200' }
   : {
       node: process.env.OPENSEARCH_URL,
       auth: {

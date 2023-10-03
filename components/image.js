1e417ba670061e489472390128932d3323c16878
ekzyis <27162016+ekzyis@users.noreply.github.com>
2023-10-03 20:05:04 +0200

    Image refactor refactor (#541)
    * Remove outdated comments about srcSet value
    
    We no longer distinguish between `undefined` and `null` for `srcSet`.
    
    * Fix misleading URL shown
    
    * Update/fix comments in <ImageOriginal>
    
    * Simplify condition when to show image
    
    I think this is not required since `showImage` will always stay false if tab !== 'preview' or me?.clickToLoadImg are true.
    
    * Rename to imgproxyOnly
    
    * Add info to imgproxyOnly setting
    
    * Fix text of markdown links not used on imgproxy errors
    
    * Fix rendering markdown links with text as images
    
    ---------
    
    Co-authored-by: ekzyis <ek@stacker.news>

--- components/image.js
+++ components/image.js
@@ -19,7 +19,7 @@ function ImageOriginal ({ src, topLevel, nofollow, tab, children, onClick, ...pr
   const [showImage, setShowImage] = useState(false)
 
   useEffect(() => {
-    if (me?.clickToLoadImg && tab !== 'preview') return
+    if (me?.imgproxyOnly && tab !== 'preview') return
     // make sure it's not a false negative by trying to load URL as <img>
     const img = new window.Image()
     img.onload = () => setShowImage(true)
@@ -31,8 +31,7 @@ function ImageOriginal ({ src, topLevel, nofollow, tab, children, onClick, ...pr
     }
   }, [src, showImage])
 
-  if (showImage && (tab === 'preview' || !me?.clickToLoadImg)) {
-    // image is still processing and user is okay with loading original url automatically
+  if (showImage) {
     return (
       <img
         className={topLevel ? styles.topLevel : undefined}
@@ -42,13 +41,17 @@ function ImageOriginal ({ src, topLevel, nofollow, tab, children, onClick, ...pr
       />
     )
   } else {
-    // image is still processing or user is not okay with loading original url automatically
+    // user is not okay with loading original url automatically or there was an error loading the image
+
+    // If element parsed by markdown is a raw URL, we use src as the text to not mislead users.
+    // This will not be the case if [text](url) format is used. Then we will show what was chosen as text.
+    const isRawURL = /^https?:\/\//.test(children?.[0])
     return (
       <a
         target='_blank'
         rel={`noreferrer ${nofollow ? 'nofollow' : ''} noopener`}
         href={src}
-      >{children || src}
+      >{isRawURL ? src : children}
       </a>
     )
   }
@@ -89,8 +92,8 @@ function ImageProxy ({ src, srcSet: srcSetObj, onClick, topLevel, onError, ...pr
 export function ZoomableImage ({ src, srcSet, ...props }) {
   const showModal = useShowModal()
 
-  // if `srcSet` is undefined, it means the image was not processed by worker yet
-  const [imgproxy, setImgproxy] = useState(srcSet || IMGPROXY_URL_REGEXP.test(src))
+  // if `srcSet` is falsy, it means the image was not processed by worker yet
+  const [imgproxy, setImgproxy] = useState(!!srcSet || IMGPROXY_URL_REGEXP.test(src))
 
   // backwards compatibility:
   // src may already be imgproxy url since we used to replace image urls with imgproxy urls

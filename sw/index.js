e9ffe4340575c463053c41682dddbf8abb076004
ekzyis <ek@ekzyis.com>
2023-10-18 01:51:32 +0200

    Fix empty push notifications

--- sw/index.js
+++ sw/index.js
@@ -77,7 +77,7 @@ self.addEventListener('push', async function (event) {
   const { tag } = payload.options
   event.waitUntil((async () => {
     // TIP and EARN notifications simply replace the previous notifications
-    if (!tag || ['TIP', 'EARN'].includes(tag.split('-')[0])) {
+    if (!tag || ['TIP', 'FORWARDEDTIP', 'EARN'].includes(tag.split('-')[0])) {
       return self.registration.showNotification(payload.title, payload.options)
     }
 

900592020fbaa6f31470cf2767f6c707ba1aaa17
ekzyis <ek@stacker.news>
2023-10-25 01:04:46 +0200

    Add Upload.paid boolean column
    One item can have multiple images linked to it, but an image can also be used in multiple items (many-to-many relation).
    
    Since we don't really care to which item an image is linked and vice versa, we just use a boolean column to mark if an image was already paid for.
    
    This makes fee calculation easier since no JOINs are required.

--- prisma/migrations/20231019162233_item_multiple_uploads/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `uploadId` on the `Item` table. All the data in the column will be lost.
-
-*/
--- DropIndex
-DROP INDEX "Upload.itemId_unique";

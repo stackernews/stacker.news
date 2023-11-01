11fa9e4d109ba2c28a58932e3515d4553aa85239
ekzyis <ek@stacker.news>
2023-11-01 23:09:37 +0100

    Fix rescheduling of other jobs
    * also update schedule from every minute to every hour

--- prisma/migrations/20231025203103_delete_unused_images_schedule/migration.sql
+++ prisma/migrations/20231025203103_delete_unused_images_schedule/migration.sql
@@ -1,23 +1,17 @@
--- add 'deleteUnusedImages' job to the prisma hack
--- see migration 20230522153900_schedule_jobs
-CREATE OR REPLACE FUNCTION create_scheduled_jobs()
+-- add 'deleteUnusedImages' job
+CREATE OR REPLACE FUNCTION create_delete_unused_images_job()
 RETURNS INTEGER
 LANGUAGE plpgsql
 AS $$
 DECLARE
 BEGIN
-    INSERT INTO pgboss.schedule (name, cron, timezone) VALUES ('trust', '0 2 * * *', 'America/Chicago') ON CONFLICT DO NOTHING;
-    INSERT INTO pgboss.schedule (name, cron, timezone) VALUES ('auction', '* * * * *', 'America/Chicago') ON CONFLICT DO NOTHING;
-    INSERT INTO pgboss.schedule (name, cron, timezone) VALUES ('earn', '0 0 * * *', 'America/Chicago') ON CONFLICT DO NOTHING;
-    INSERT INTO pgboss.schedule (name, cron, timezone) VALUES ('streak', '15 0 * * *','America/Chicago') ON CONFLICT DO NOTHING;
-    INSERT INTO pgboss.schedule (name, cron, timezone) VALUES ('views', '0 0 * * *', 'America/Chicago') ON CONFLICT DO NOTHING;
-    INSERT INTO pgboss.schedule (name, cron, timezone) VALUES ('rankViews', '* * * * *', 'America/Chicago') ON CONFLICT DO NOTHING;
-    INSERT INTO pgboss.schedule (name, cron, timezone) VALUES ('rankViews', '* * * * *', 'America/Chicago') ON CONFLICT DO NOTHING;
-    INSERT INTO pgboss.schedule (name, cron, timezone) VALUES ('deleteUnusedImages', '* * * * *', 'America/Chicago') ON CONFLICT DO NOTHING;
+    INSERT INTO pgboss.schedule (name, cron, timezone) VALUES ('deleteUnusedImages', '0 * * * *', 'America/Chicago') ON CONFLICT DO NOTHING;
     return 0;
 EXCEPTION WHEN OTHERS THEN
     return 0;
 END;
 $$;
 
-SELECT create_scheduled_jobs();
+SELECT create_delete_unused_images_job();
+
+DROP FUNCTION create_delete_unused_images_job;

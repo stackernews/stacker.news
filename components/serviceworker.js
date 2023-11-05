5dfeb700bc03dd1949706dd54a9e99aadb541d07
ekzyis <27162016+ekzyis@users.noreply.github.com>
2023-11-05 22:08:44 +0100

    Resubscribe if service worker lost push subscription (#597)
    * Also delete push subscription in IndexedDB
    
    * Fix pushsubscriptionchange function signature
    
    * Send SYNC_SUBSCRIPTION after successful registration
    
    * Resubscribe if service worker lost subscription
    
    ---------
    
    Co-authored-by: ekzyis <ek@stacker.news>

--- components/serviceworker.js
+++ components/serviceworker.js
@@ -88,6 +88,9 @@ export const ServiceWorkerProvider = ({ children }) => {
     const { endpoint } = subscription
     logger.info('unsubscribed from push notifications', { endpoint })
     await deletePushSubscription({ variables: { endpoint } })
+    // also delete push subscription in IndexedDB so we can tell if the user disabled push subscriptions
+    // or we lost the push subscription due to a bug
+    navigator.serviceWorker.controller.postMessage({ action: 'DELETE_SUBSCRIPTION' })
     logger.info('deleted push subscription from server', { endpoint })
   }
 
@@ -106,24 +109,36 @@ export const ServiceWorkerProvider = ({ children }) => {
       pushManager: 'PushManager' in window
     })
     setPermission({ notification: 'Notification' in window ? window.Notification.permission : 'denied' })
-    // since (a lot of) browsers don't support the pushsubscriptionchange event,
-    // we sync with server manually by checking on every page reload if the push subscription changed.
-    // see https://medium.com/@madridserginho/how-to-handle-webpush-api-pushsubscriptionchange-event-in-modern-browsers-6e47840d756f
-    navigator?.serviceWorker?.controller?.postMessage?.({ action: 'SYNC_SUBSCRIPTION' })
-    logger.info('sent SYNC_SUBSCRIPTION to service worker')
-  }, [])
 
-  useEffect(() => {
-    if (!support.serviceWorker) {
+    if (!('serviceWorker' in navigator)) {
       logger.info('device does not support service worker')
       return
     }
+
     const wb = new Workbox('/sw.js', { scope: '/' })
     wb.register().then(registration => {
       logger.info('service worker registration successful')
       setRegistration(registration)
     })
-  }, [support.serviceWorker])
+  }, [])
+
+  useEffect(() => {
+    // wait until successful registration
+    if (!registration) return
+    // setup channel between app and service worker
+    const channel = new MessageChannel()
+    navigator?.serviceWorker?.controller?.postMessage({ action: 'ACTION_PORT' }, [channel.port2])
+    channel.port1.onmessage = (event) => {
+      if (event.data.action === 'RESUBSCRIBE') {
+        return subscribeToPushNotifications()
+      }
+    }
+    // since (a lot of) browsers don't support the pushsubscriptionchange event,
+    // we sync with server manually by checking on every page reload if the push subscription changed.
+    // see https://medium.com/@madridserginho/how-to-handle-webpush-api-pushsubscriptionchange-event-in-modern-browsers-6e47840d756f
+    navigator?.serviceWorker?.controller?.postMessage?.({ action: 'SYNC_SUBSCRIPTION' })
+    logger.info('sent SYNC_SUBSCRIPTION to service worker')
+  }, [registration])
 
   return (
     <ServiceWorkerContext.Provider value={{ registration, support, permission, requestNotificationPermission, togglePushSubscription }}>

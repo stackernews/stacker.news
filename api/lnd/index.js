812a0041fe229babaf756f7e6d9687b2ed3d27db
MeuAmigoPedro <85800864+meuamigopedro@users.noreply.github.com>
2023-12-23 22:40:47 -0300

    Update index.js
    Key improvements:
    
    	1.	Destructuring of Environment Variables:
    I used destructuring to make the code more concise and readable.
    	2.	Checking Environment Variables:
    Added a check to ensure that all required environment variables are present. If any are missing, the script displays an error message and exits.
    	3.	Improved Error Handling:
    Enhanced error handling by displaying detailed error messages and exiting the script if there is a problem during authentication or connection checking with LND.

--- api/lnd/index.js
+++ api/lnd/index.js
@@ -1,18 +1,25 @@
-import lndService from 'ln-service'
+import lndService from 'ln-service';
 
-const { lnd } = lndService.authenticatedLndGrpc({
-  cert: process.env.LND_CERT,
-  macaroon: process.env.LND_MACAROON,
-  socket: process.env.LND_SOCKET
-})
+// Destructure environment variables
+const { LND_CERT, LND_MACAROON, LND_SOCKET } = process.env;
 
-// Check LND GRPC connection
+// Check if all required environment variables are present
+if (!LND_CERT || !LND_MACAROON || !LND_SOCKET) {
+  console.error('Make sure to set the required environment variables.');
+  process.exit(1);
+}
+
+// Authenticate LND gRPC
+const { lnd } = lndService.authenticatedLndGrpc({ cert: LND_CERT, macaroon: LND_MACAROON, socket: LND_SOCKET });
+
+// Check the connection to LND GRPC
 lndService.getWalletInfo({ lnd }, (err, result) => {
   if (err) {
-    console.error('LND GRPC connection error')
-    return
+    console.error('Error connecting to LND GRPC:', err);
+    process.exit(1);
   }
-  console.log('LND GRPC connection successful')
-})
+  console.log('Successful connection to LND GRPC');
+});
 
-export default lnd
+// Export the authenticated LND object
+export default lnd;

f20e43afed266a9b6c1a5ea9e12c13128afa5b4f
mzivil <158518982+mzivil@users.noreply.github.com>
2024-02-08 12:42:54 -0500

    filter nsfw subs based on user preference

--- api/resolvers/sub.js
+++ api/resolvers/sub.js
@@ -84,21 +84,24 @@ export default {
     sub: getSub,
     subs: async (parent, args, { models, me }) => {
       if (me) {
-        return await models.$queryRaw`
+        const currentUser = await models.user.findUnique({ where: { id: me.id } })
+        const showNsfw = currentUser ? currentUser.nsfwMode : false
+        return await models.$queryRawUnsafe(`
           SELECT "Sub".*, COALESCE(json_agg("MuteSub".*) FILTER (WHERE "MuteSub"."userId" IS NOT NULL), '[]') AS "MuteSub"
           FROM "Sub"
           LEFT JOIN "MuteSub" ON "Sub".name = "MuteSub"."subName" AND "MuteSub"."userId" = ${me.id}::INTEGER
-          WHERE status <> 'STOPPED'
+          WHERE status <> 'STOPPED' ${showNsfw ? '' : 'AND "Sub"."nsfw" = FALSE'}
           GROUP BY "Sub".name, "MuteSub"."userId"
           ORDER BY "Sub".name ASC
-        `
+        `)
       }
 
       return await models.sub.findMany({
         where: {
           status: {
             not: 'STOPPED'
-          }
+          },
+          nsfw: false
         },
         orderBy: {
           name: 'asc'

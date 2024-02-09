9887ec5ef267d0bcce05b8730ddd6985c5673423
mzivil <158518982+mzivil@users.noreply.github.com>
2024-02-09 10:51:56 -0500

    fix relationClause to join with root item

--- api/resolvers/item.js
+++ api/resolvers/item.js
@@ -152,22 +152,22 @@ const relationClause = (type) => {
   let clause = ''
   switch (type) {
     case 'comments':
-      clause += ' FROM "Item" JOIN "Item" root ON "Item"."rootId" = root.id '
+      clause += ' FROM "Item" JOIN "Item" root ON "Item"."rootId" = root.id LEFT JOIN "Sub" ON "Sub"."name" = root."subName" '
       break
     case 'bookmarks':
-      clause += ' FROM "Item" JOIN "Bookmark" ON "Bookmark"."itemId" = "Item"."id" '
+      clause += ' FROM "Item" JOIN "Bookmark" ON "Bookmark"."itemId" = "Item"."id" LEFT JOIN "Sub" ON "Sub"."name" = "Item"."subName" '
       break
     case 'outlawed':
     case 'borderland':
     case 'freebies':
     case 'all':
-      clause += ' FROM "Item" LEFT JOIN "Item" root ON "Item"."rootId" = root.id '
+      clause += ' FROM "Item" LEFT JOIN "Item" root ON "Item"."rootId" = root.id LEFT JOIN "Sub" ON "Sub"."name" = root."subName" '
       break
     default:
-      clause += ' FROM "Item" '
+      clause += ' FROM "Item" LEFT JOIN "Sub" ON "Sub"."name" = "Item"."subName" '
   }
 
-  return clause + ' LEFT JOIN "Sub" ON "Sub"."name" = "Item"."subName" '
+  return clause
 }
 
 const selectClause = (type) => type === 'bookmarks'

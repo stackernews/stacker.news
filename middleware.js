e6f438b88ef57f654fa49268219ea4f8c2408d2c
ekzyis <ek@stacker.news>
2024-02-09 21:36:14 +0100

    Basic CSP with unsafe-inline, unsafe-eval

--- middleware.js
+++ middleware.js
@@ -1,8 +1,8 @@
 import { NextResponse } from 'next/server'
 
-export function middleware (request) {
-  const regex = /(\/.*)?\/r\/([\w_]+)/
-  const m = regex.exec(request.nextUrl.pathname)
+const referrerRegex = /(\/.*)?\/r\/([\w_]+)/
+function referrerMiddleware (request) {
+  const m = referrerRegex.exec(request.nextUrl.pathname)
 
   const url = new URL(m[1] || '/', request.url)
   url.search = request.nextUrl.search
@@ -13,6 +13,37 @@ export function middleware (request) {
   return resp
 }
 
+export function middleware (request) {
+  let resp = NextResponse.next()
+  if (referrerRegex.test(request.nextUrl.pathname)) {
+    resp = referrerMiddleware(request)
+  }
+
+  const cspHeader = [
+    "default-src 'none'",
+    "font-src 'self'",
+    'img-src https: data:',
+    "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
+    "style-src 'self' 'unsafe-inline'",
+    "manifest-src 'self'",
+    'connect-src https: wss:',
+    'upgrade-insecure-requests'
+  ].join('; ')
+
+  resp.headers.set('Content-Security-Policy', cspHeader)
+  return resp
+}
+
 export const config = {
-  matcher: ['/(.*/|)r/([\\w_]+)([?#]?.*)']
+  matcher: [
+    // NextJS recommends to not add the CSP header to prefetches and static assets
+    // See https://nextjs.org/docs/app/building-your-application/configuring/content-security-policy
+    {
+      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+      missing: [
+        { type: 'header', key: 'next-router-prefetch' },
+        { type: 'header', key: 'purpose', value: 'prefetch' }
+      ]
+    }
+  ]
 }

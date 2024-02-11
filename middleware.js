085507e989517d63e46f89ec7d334338816ccc86
ekzyis <ek@stacker.news>
2024-02-11 20:53:37 +0100

    Allow youtube and twitter embeds
    For some reason, www.youtube.com is enough. It also works for youtube.com and youtube-nocookie.com.
    
    For twitter embeds from twitter.com or x.com, platform.twitter.com is enough.

--- middleware.js
+++ middleware.js
@@ -34,6 +34,7 @@ export function middleware (request) {
     // unsafe-inline for styles is not ideal but okay if script-src is using nonces
     "style-src 'self' 'unsafe-inline'",
     "manifest-src 'self'",
+    'frame-src www.youtube.com platform.twitter.com',
     "connect-src 'self' https: wss:",
     // disable dangerous plugins like Flash
     "object-src 'none'",

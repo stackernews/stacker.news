022ed8b2a5132b0ec1b70c127d8bc290fdacee73
ekzyis <ek@stacker.news>
2024-02-12 11:43:34 +0100

    Fix query returns number of rows instead of rows

--- worker/wallet.js
+++ worker/wallet.js
@@ -257,7 +257,7 @@ async function checkWithdrawal ({ data: { hash }, boss, models, lnd }) {
 
 export async function autoDropBolt11s ({ models, lnd }) {
   // This query will update the withdrawls and return what the hash and bol11 values were before the update
-  const invoices = await serialize(models, models.$executeRaw`
+  const invoices = await serialize(models, models.$queryRaw`
     WITH to_be_updated AS (
       SELECT id, hash, bolt11
       FROM "Withdrawl"

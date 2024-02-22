448f9b5c67ad0ba98f4f1dd33b22acef40aebb72
ekzyis <ek@stacker.news>
2024-02-23 00:48:01 +0100

    Don't drop noteTerritoryPosts column

--- prisma/migrations/20240222032317_drop_territory_notification_settings/migration.sql
+++ prisma/migrations/20240222032317_drop_territory_notification_settings/migration.sql
@@ -1,15 +1,7 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `noteTerritoryPosts` on the `users` table. All the data in the column will be lost.
-
-*/
-
 -- migrate values from founders which had notifications for territory posts enabled to new table
 INSERT INTO "SubSubscription"("userId", "subName")
 SELECT u.id, s.name
 FROM users u JOIN "Sub" s ON u.id = s."userId"
 WHERE "noteTerritoryPosts";
 
--- AlterTable
-ALTER TABLE "users" DROP COLUMN "noteTerritoryPosts";
+-- we don't drop the users.noteTerritoryPosts column in this migration since it's a backwards incompatible change

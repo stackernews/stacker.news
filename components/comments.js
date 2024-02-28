c1c75e954c58b86849c6bfd02c7b8a43772ae1fd
mzivil <158518982+mzivil@users.noreply.github.com>
2024-02-28 17:38:29 -0500

    add useNewCommentFetcher to fetch and save new comments to apollo cache

--- components/comments.js
+++ components/comments.js
@@ -1,4 +1,4 @@
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
 import Comment, { CommentSkeleton } from './comment'
 import styles from './header.module.css'
 import Nav from 'react-bootstrap/Nav'
@@ -6,6 +6,10 @@ import Navbar from 'react-bootstrap/Navbar'
 import { numWithUnits } from '../lib/format'
 import { defaultCommentSort } from '../lib/item'
 import { useRouter } from 'next/router'
+import { NEW_COMMENTS, ITEM_WITH_COMMENTS } from '../fragments/comments'
+import { useApolloClient, useQuery } from '@apollo/client'
+import { SSR } from '../lib/constants'
+import { ITEM_FULL } from '../fragments/items'
 
 export function CommentsHeader ({ handleSort, pinned, bio, parentCreatedAt, commentSats }) {
   const router = useRouter()
@@ -60,8 +64,9 @@ export function CommentsHeader ({ handleSort, pinned, bio, parentCreatedAt, comm
   )
 }
 
-export default function Comments ({ parentId, pinned, bio, parentCreatedAt, commentSats, comments, ...props }) {
+export default function Comments ({ parentId, pinned, bio, parentCreatedAt, commentSats, comments, lastCommentAt, newComments, ...props }) {
   const router = useRouter()
+  useNewCommentFetcher(parentId, lastCommentAt || parentCreatedAt)
 
   const pins = comments?.filter(({ position }) => !!position).sort((a, b) => a.position - b.position)
 
@@ -97,4 +102,83 @@ export default function Comments ({ parentId, pinned, bio, parentCreatedAt, comm
 
 export function CommentsSkeleton () {
   return <CommentSkeleton skeletonChildren={7} />
+}
+
+function useNewCommentFetcher (rootId, afterTimestamp) {
+  const client = useApolloClient()
+  const [newCommentsLastCheckedAt, setNewCommentsLastCheckedAt] = useState(afterTimestamp)
+  const { data } = useQuery(NEW_COMMENTS, SSR
+    ? { ssr: false }
+    : {
+        pollInterval: 10000,
+        variables: { rootId, after: newCommentsLastCheckedAt }
+      })
+
+  if (data && data.newComments) {
+    saveNewCommentsToCache(client, rootId, data.newComments)
+    const latestCommentCreatedAt = findLatestCommentCreatedAt(data.newComments)
+    if (latestCommentCreatedAt) {
+      setNewCommentsLastCheckedAt(latestCommentCreatedAt)
+    }
+  }
+
+  return null
+}
+
+function saveNewCommentsToCache (client, rootId, newComments) {
+  for (const newComment of newComments) {
+    const parentId = newComment.parentId.toString()
+    // New top-level comments must use updateQuery while new nested comments
+    // must use updateFragment to update the correct view components
+    if (parentId === rootId) {
+      client.cache.updateQuery({
+        query: ITEM_FULL,
+        variables: { id: rootId }
+      }, (data) => {
+        if (!data) return data
+        const { item } = data
+
+        const existingNewComments = item.newComments || []
+        const isAlreadyInNewComments = existingNewComments.some(existing => existing.id === newComment.id)
+        const updatedNewComments = isAlreadyInNewComments ? existingNewComments : existingNewComments.concat(newComment)
+        // Filter new comments that may already be in the item's comments
+        const filteredNewComments = updatedNewComments.filter((comment) => !item.comments.some(existing => existing.id === comment.id))
+        return {
+          item: {
+            ...item,
+            newComments: filteredNewComments
+          },
+        }
+      })
+    } else {
+      client.cache.updateFragment({
+        id: `Item:${parentId}`,
+        fragmentName: 'ItemWithComments',
+        fragment: ITEM_WITH_COMMENTS
+      }, (data) => {
+        if (!data) return data
+
+        const existingNewComments = data.newComments || []
+        const isAlreadyInNewComments = existingNewComments.some(existing => existing.id === newComment.id)
+        const updatedNewComments = isAlreadyInNewComments ? existingNewComments : existingNewComments.concat(newComment)
+        // Filter new comments that may already be in the item's comments
+        const filteredNewComments = updatedNewComments.filter((comment) => !data.comments.some(existing => existing.id === comment.id))
+        return {
+          ...data,
+          newComments: filteredNewComments
+        }
+      })
+    }
+  }
+}
+
+function findLatestCommentCreatedAt (comments) {
+  if (comments.length === 0) return null
+  let latest = comments[0].createdAt
+  for (const comment of comments) {
+    if (comment.createdAt > latest) {
+      latest = comment.createdAt
+    }
+  }
+  return latest
 }

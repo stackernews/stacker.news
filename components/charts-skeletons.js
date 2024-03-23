263d0cc425c69e57da84d3fe0652047e1194951f
Satoshi Nakamoto <WeAreAllSatoshiNakamoto@proton.me>
2024-03-23 12:27:00 -0400

    remove duplicative styles

--- components/charts-skeletons.js
+++ components/charts-skeletons.js
@@ -1,19 +1,19 @@
-export function GrowthPieChartSkeleton ({ width = '100%', height = '250px !important', minWidth = '200px' }) {
-  return <ChartSkeleton {...{ width, height, minWidth }} />
+export function GrowthPieChartSkeleton ({ height = '250px', minWidth = '200px' }) {
+  return <ChartSkeleton {...{ height, minWidth }} />
 }
 
-export function WhenComposedChartSkeleton ({ width = '100%', height = '300px !important', minWidth = '300px' }) {
-  return <ChartSkeleton {...{ width, height, minWidth }} />
+export function WhenComposedChartSkeleton ({ height = '300px', minWidth = '300px' }) {
+  return <ChartSkeleton {...{ height, minWidth }} />
 }
 
-export function WhenAreaChartSkeleton ({ width = '100%', height = '300px !important', minWidth = '300px' }) {
-  return <ChartSkeleton {...{ width, height, minWidth }} />
+export function WhenAreaChartSkeleton ({ height = '300px', minWidth = '300px' }) {
+  return <ChartSkeleton {...{ height, minWidth }} />
 }
 
-export function WhenLineChartSkeleton ({ width = '100%', height = '300px !important', minWidth = '300px' }) {
-  return <ChartSkeleton {...{ width, height, minWidth }} />
+export function WhenLineChartSkeleton ({ height = '300px', minWidth = '300px' }) {
+  return <ChartSkeleton {...{ height, minWidth }} />
 }
 
 function ChartSkeleton (props) {
-  return <div className='h-auto mx-auto w-100 clouds' style={{ ...props }} />
+  return <div className='mx-auto w-100 clouds' style={{ ...props }} />
 }

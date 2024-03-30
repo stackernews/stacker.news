62c39db47c5b0932143e26c2bed55c14ba3631ac
ekzyis <ek@stacker.news>
2024-03-30 02:55:15 +0100

    Add log.module.css

--- pages/wallet/logs.js
+++ pages/wallet/logs.js
@@ -6,6 +6,7 @@ import { getGetServerSideProps } from '@/api/ssrApollo'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { datePivot } from '@/lib/time'
+import styles from '@/styles/log.module.css'
 
 export const getServerSideProps = getGetServerSideProps({ query: null })
 
@@ -33,15 +34,15 @@ export default function WalletLogs () {
       <CenterLayout>
         <h2 className='text-center'>wallet logs</h2>
         <div>
-          <div className='ms-3 text-muted'>
+          <div className={styles.header}>
             <span>show earlier logs:</span>
             <Link href={`/wallet/logs?since=${earlierTs5m}`} className='mx-1 text-muted text-underline'>5m</Link>
             <Link href={`/wallet/logs?since=${earlierTs1h}`} className='mx-1 text-muted text-underline'>1h</Link>
             <Link href={`/wallet/logs?since=${earlierTs6h}`} className='mx-1 text-muted text-underline'>6h</Link>
+            <span className='mx-1 text-muted' suppressHydrationWarning>{new Date(more ? sinceRounded : logStart).toLocaleTimeString()}</span>
           </div>
-          <div>
-            {!more && <div className='mx-3 text-muted'>------ end of logs ------</div>}
-            <div className='mx-3 text-muted' suppressHydrationWarning>{new Date(more ? sinceRounded : logStart).toLocaleTimeString()}</div>
+          <div className={styles.logBox}>
+            {!more && <div className={styles.eol}>------ end of logs ------</div>}
             {logs.map((log, i) => <LogMessage key={i} {...log} />)}
           </div>
         </div>

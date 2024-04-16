aacd5ce44bba57483616ef8685aa5017c06ff4d4
Satoshi Nakamoto <WeAreAllSatoshiNakamoto@proton.me>
2024-04-15 21:47:47 -0400

    short circuit return empty payload for anon requesting bookmarks

--- api/resolvers/search.js
+++ api/resolvers/search.js
@@ -176,7 +176,10 @@ export default {
       let sitems = null
       let termQueries = []
 
-      if (!q) {
+      // short circuit: return empty result if either:
+      // 1. no query provided, or
+      // 2. searching bookmarks without being authed
+      if (!q || (what === 'bookmarks' && !me)) {
         return {
           items: [],
           cursor: null

393fede5103e8115d37994a41404fec76730ee28
benalleng <benalleng@gmail.com>
2024-04-18 09:08:51 -0400

    fix: add 200ms delay to scroll in capture worker

--- capture/index.js
+++ capture/index.js
@@ -92,8 +92,10 @@ app.get('/*', async (req, res) => {
     console.timeLog(timeLabel, 'page loaded')
 
     if (commentId) {
-      console.timeLog(timeLabel, 'scrolling to comment')
       await page.waitForSelector('.outline-it')
+      setTimeout(() => {
+        console.timeLog(timeLabel, 'scrolling to comment')
+      }, 200)
     }
 
     const file = await page.screenshot({ type: 'png', captureBeyondViewport: false })

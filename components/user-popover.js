9e8fb7a1cb3385d571e6975ec622f9950a8259df
Felipe Bueno <bueno.felipe@gmail.com>
2024-04-23 17:30:28 -0300

    UserDetails -> StackingSince on UserPopover

--- components/user-popover.js
+++ components/user-popover.js
@@ -1,40 +1,18 @@
 import { USER_FULL } from '@/fragments/users'
 import Moon from '@/svgs/moon-fill.svg'
-import CodeIcon from '@/svgs/terminal-box-fill.svg'
 import { useQuery } from '@apollo/client'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import { Popover } from 'react-bootstrap'
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
-import { SocialLink } from './user-header'
 import { User } from './user-list'
 
-function UserDetails ({ user }) {
+function StackingSince ({ since }) {
   return (
-    <div className='d-flex flex-column'>
-      <small className='text-muted d-flex-inline'>stacking since: {user.since
-        ? <Link href={`/items/${user.since}`} className='ms-1'>#{user.since}</Link>
-        : <span>never</span>}
-      </small>
-      {user.optional.maxStreak !== null &&
-        <small className='text-muted d-flex-inline'>longest cowboy streak: {user.optional.maxStreak}</small>}
-      {user.optional.isContributor &&
-        <small className='text-muted d-flex align-items-center'>
-          <CodeIcon className='me-1' height={16} width={16} /> verified stacker.news contributor
-        </small>}
-      {user.optional.nostrAuthPubkey &&
-        <small className='text-muted d-flex-inline'>
-          <SocialLink name='Nostr' id={user.optional.nostrAuthPubkey} />
-        </small>}
-      {user.optional.githubId &&
-        <small className='text-muted d-flex-inline'>
-          <SocialLink name='Github' id={user.optional.githubId} />
-        </small>}
-      {user.optional.twitterId &&
-        <small className='text-muted d-flex-inline'>
-          <SocialLink name='Twitter' id={user.optional.twitterId} />
-        </small>}
-    </div>
+    <small className='text-muted d-flex-inline'>stacking since: {since
+      ? <Link href={`/items/${since}`}>#{since}</Link>
+      : <span>never</span>}
+    </small>
   )
 }
 
@@ -66,7 +44,7 @@ export default function UserPopover ({ text, children }) {
           <Popover.Body style={{ fontWeight: 500, fontSize: '.9rem' }}>
             {user
               ? (
-                <User user={user} Embellish={() => <UserDetails user={user} />} />
+                <User user={user} Embellish={() => <StackingSince since={user.since} />} />
                 )
               : (
                 <Moon className='spin fill-grey' />

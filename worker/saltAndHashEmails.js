60e6f8a32bc7cfbcbf9c0e604fa1579f3a274573
Satoshi Nakamoto <WeAreAllSatoshiNakamoto@proton.me>
2024-04-27 10:49:30 -0400

    fix salt and hash raw sql statement

--- worker/saltAndHashEmails.js
+++ worker/saltAndHashEmails.js
@@ -1,7 +1,7 @@
 export async function saltAndHashEmails ({ models }) {
   try {
     console.log('Migrating existing emails to salt and hash them...')
-    await models.$queryRaw`select migrate_existing_user_emails(${process.env.EMAIL_SALT}::TEXT)`
+    await models.$executeRaw`select migrate_existing_user_emails(${process.env.EMAIL_SALT})`
     console.log('Successfully migrated existing emails to salt and hash them!')
   } catch (err) {
     console.error('Error occurred while salting and hashing existing emails')

e7259d23f71982ba0561ffd244cb0eb3234c5a29
Satoshi Nakamoto <WeAreAllSatoshiNakamoto@proton.me>
2024-04-30 14:39:42 -0400

    consolidate console.error logs

--- worker/saltAndHashEmails.js
+++ worker/saltAndHashEmails.js
@@ -4,7 +4,6 @@ export async function saltAndHashEmails ({ models }) {
     await models.$executeRaw`select migrate_existing_user_emails(${process.env.EMAIL_SALT})`
     console.log('Successfully migrated existing emails to salt and hash them!')
   } catch (err) {
-    console.error('Error occurred while salting and hashing existing emails')
-    console.error(err)
+    console.error('Error occurred while salting and hashing existing emails:', err)
   }
 }

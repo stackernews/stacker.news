668fe3c75b506dfb20168c02e44a74c72a34f961
keyan <keyan.kousha+huumn@gmail.com>
2024-05-03 16:34:36 -0500

    refine user popover

--- components/user-popover.js
+++ components/user-popover.js
@@ -1,19 +1,21 @@
 import { USER } from '@/fragments/users'
 import errorStyles from '@/styles/error.module.css'
-import Moon from '@/svgs/moon-fill.svg'
 import { useLazyQuery } from '@apollo/client'
 import Link from 'next/link'
 import { useRef, useState } from 'react'
 import { Popover } from 'react-bootstrap'
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
-import { User } from './user-list'
+import { UserBase, UserSkeleton } from './user-list'
 import styles from './user-popover.module.css'
+import classNames from 'classnames'
 
 function StackingSince ({ since }) {
   return (
-    <small className='text-muted d-flex-inline'>stacking since: {since
-      ? <Link href={`/items/${since}`}>#{since}</Link>
-      : <span>never</span>}
+    <small className='text-muted d-flex-inline'>
+      stacking since:{' '}
+      {since
+        ? <Link href={`/items/${since}`}>#{since}</Link>
+        : <span>never</span>}
     </small>
   )
 }
@@ -21,11 +23,11 @@ function StackingSince ({ since }) {
 export default function UserPopover ({ name, children }) {
   const [showOverlay, setShowOverlay] = useState(false)
 
-  const [getUser, query] = useLazyQuery(
+  const [getUser, { loading, data }] = useLazyQuery(
     USER,
     {
       variables: { name },
-      nextFetchPolicy: 'cache-and-network'
+      fetchPolicy: 'cache-first'
     }
   )
 
@@ -33,20 +35,21 @@ export default function UserPopover ({ name, children }) {
 
   const handleMouseEnter = () => {
     clearTimeout(timeoutId.current)
+    getUser()
     timeoutId.current = setTimeout(() => {
       setShowOverlay(true)
-      getUser()
-    }, 777)
+    }, 500)
   }
 
   const handleMouseLeave = () => {
     clearTimeout(timeoutId.current)
-    timeoutId.current = setTimeout(() => setShowOverlay(false), 333)
+    timeoutId.current = setTimeout(() => setShowOverlay(false), 100)
   }
 
   return (
     <OverlayTrigger
       show={showOverlay}
+      placement='bottom'
       onHide={handleMouseLeave}
       overlay={
         <Popover
@@ -55,11 +58,15 @@ export default function UserPopover ({ name, children }) {
           className={styles.userPopover}
         >
           <Popover.Body className={styles.userPopBody}>
-            {!query.data || query.loading
-              ? <Moon className='spin fill-grey' />
-              : !query.data.user
-                  ? <h1 className={[errorStyles.status, errorStyles.describe].join(' ')}>USER NOT FOUND</h1>
-                  : <User user={query.data.user} Embellish={() => <StackingSince since={query.data.user.since} />} />}
+            {!data || loading
+              ? <UserSkeleton />
+              : !data.user
+                  ? <h1 className={classNames(errorStyles.status, errorStyles.describe)}>USER NOT FOUND</h1>
+                  : (
+                    <UserBase user={data.user} className='mb-0 pb-0'>
+                      <StackingSince since={data.user.since} />
+                    </UserBase>
+                    )}
           </Popover.Body>
         </Popover>
       }

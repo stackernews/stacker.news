42891ea98668aa0745529ab779f94ea480b8cd1d
Tom Smith <tomdsmith1@gmail.com>
2024-05-17 08:52:58 +0100

    Restore lightning in filter

--- pages/settings/index.js
+++ pages/settings/index.js
@@ -682,7 +682,7 @@ function AuthMethods ({ methods, apiKeyEnabled }) {
     const activatedAuths = providers
       .filter(key => !!methods[key])
 
-    return activatedAuths.length === 1 && activatedAuths[0] === 'email'
+    return activatedAuths.length === 1 && activatedAuths[0] === 'lightning'
   }
 
   const unlink = async type => {

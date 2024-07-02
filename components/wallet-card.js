d7c81cfa9fa23276513847dac191d25e4d7af13e
ekzyis <ek@stacker.news>
2024-07-02 05:38:41 +0200

    Fix sendPayment called with empty config
    * removed useEffect such that config is available on first render
    * fix hydration error using dynamic import without SSR

--- components/wallet-card.js
+++ components/wallet-card.js
@@ -1,10 +1,8 @@
-import { Badge, Button, Card } from 'react-bootstrap'
+import { Badge, Card } from 'react-bootstrap'
 import styles from '@/styles/wallet.module.css'
 import Plug from '@/svgs/plug.svg'
 import Gear from '@/svgs/settings-5-fill.svg'
 import Link from 'next/link'
-import CancelButton from './cancel-button'
-import { SubmitButton } from './form'
 import { useWallet, Status } from './wallet'
 
 export function WalletCard ({ name, title, badges, status }) {
@@ -49,25 +47,5 @@ export function WalletCard ({ name, title, badges, status }) {
         </Card.Footer>
       </Link>
     </Card>
-  )
-}
-
-export function WalletButtonBar ({
-  wallet, disable,
-  className, children, onDelete, onCancel, hasCancel = true,
-  createText = 'attach', deleteText = 'detach', editText = 'save'
-}) {
-  return (
-    <div className={`mt-3 ${className}`}>
-      <div className='d-flex justify-content-between'>
-        {wallet.isConfigured &&
-          <Button onClick={onDelete} variant='grey-medium'>{deleteText}</Button>}
-        {children}
-        <div className='d-flex align-items-center ms-auto'>
-          {hasCancel && <CancelButton onClick={onCancel} />}
-          <SubmitButton variant='primary' disabled={disable}>{wallet.isConfigured ? editText : createText}</SubmitButton>
-        </div>
-      </div>
-    </div>
   )
 }

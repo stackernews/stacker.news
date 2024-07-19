ca641cbea0dd6d45084fcd45a6cb0549cfd6836e
ekzyis <ek@stacker.news>
2024-07-19 09:42:41 -0500

    Fix missing space after =

--- wallets/cln/index.js
+++ wallets/cln/index.js
@@ -39,7 +39,7 @@ export const card = {
   badges: ['receive only', 'non-custodialish']
 }
 
-export const yupSchema =CLNAutowithdrawSchema
+export const yupSchema = CLNAutowithdrawSchema
 
 export const walletType = 'CLN'
 

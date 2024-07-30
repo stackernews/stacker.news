4fb250c0feb3cd3cf1fd65ee4f68c5b8bde11345
ekzyis <ek@stacker.news>
2024-07-30 13:33:37 -0500

    Fix missing fragment and query update

--- api/typeDefs/price.js
+++ api/typeDefs/price.js
@@ -2,6 +2,6 @@ import { gql } from 'graphql-tag'
 
 export default gql`
   extend type Query {
-    price(fiatCurrency: String): Float
+    price(fiatCurrency: String, changedCurrency: Boolean): Float
   }
 `
--- fragments/price.js
+++ fragments/price.js
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client'
 
 export const PRICE = gql`
-  query price($fiatCurrency: String) {
-    price(fiatCurrency: $fiatCurrency)
+  query price($fiatCurrency: String, $changedCurrency: Boolean) {
+    price(fiatCurrency: $fiatCurrency, changedCurrency: $changedCurrency)
   }`

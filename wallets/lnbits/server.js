d0c47a72b800a30584fdb4d72735d8585ed135d2
ekzyis <ek@stacker.news>
2024-08-07 15:23:32 -0500

    Remove hardcoded LNbits url on server again

--- wallets/lnbits/server.js
+++ wallets/lnbits/server.js
@@ -11,7 +11,6 @@ async function _createInvoice ({ url, invoiceKey, amount, expiry }, { me }) {
   const memo = me.hideInvoiceDesc ? undefined : 'autowithdraw to LNbits from SN'
   const body = JSON.stringify({ amount, unit: 'sat', expiry, memo, out: false })
 
-  url = 'http://lnbits:5000'
   const res = await fetch(url + path, { method: 'POST', headers, body })
   if (!res.ok) {
     const errBody = await res.json()

3cd9991aa9e4a8db29905c64ee8764fca26d33f3
ekzyis <ek@stacker.news>
2024-08-16 09:02:09 -0500

    Hide wallet name in logs for only a single wallet (#1305)

--- components/log-message.js
+++ components/log-message.js
@@ -1,13 +1,13 @@
 import { timeSince } from '@/lib/time'
 import styles from './log-message.module.css'
 
-export default function LogMessage ({ wallet, level, message, ts }) {
+export default function LogMessage ({ showWallet, wallet, level, message, ts }) {
   level = level.toLowerCase()
   const levelClassName = ['ok', 'success'].includes(level) ? 'text-success' : level === 'error' ? 'text-danger' : level === 'info' ? 'text-info' : ''
   return (
     <tr className={styles.line}>
       <td className={styles.timestamp}>{timeSince(new Date(ts))}</td>
-      <td className={styles.wallet}>[{wallet}]</td>
+      {showWallet ? <td className={styles.wallet}>[{wallet}]</td> : <td className='mx-1' />}
       <td className={`${styles.level} ${levelClassName}`}>{level === 'success' ? 'ok' : level}</td>
       <td>{message}</td>
     </tr>

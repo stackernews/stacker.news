2bf70bdd7021edd9a5e01bce826f795e53c755e2
ekzyis <ek@stacker.news>
2024-08-17 00:18:58 -0500

    Move time functions into lib/time

--- components/nostr-auth.js
+++ components/nostr-auth.js
@@ -8,7 +8,7 @@ import { useRouter } from 'next/router'
 import AccordianItem from './accordian-item'
 import BackIcon from '@/svgs/arrow-left-line.svg'
 import styles from './lightning-auth.module.css'
-import { callWithTimeout } from '@/lib/nostr'
+import { callWithTimeout } from '@/lib/time'
 
 function ExtensionError ({ message, details }) {
   return (

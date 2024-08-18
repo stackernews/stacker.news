f9bf1b24123482f3502dafc31f5fcd628c1ddd56
ekzyis <ek@stacker.news>
2024-08-18 02:57:26 -0500

    Add timeout to NWC test invoice

--- wallets/nwc/server.js
+++ wallets/nwc/server.js
@@ -1,9 +1,10 @@
+import { withTimeout } from '@/lib/time'
 import { hasMethod, nwcCall } from 'wallets/nwc'
 export * from 'wallets/nwc'
 
 export async function testConnectServer ({ nwcUrlRecv }) {
   await hasMethod(nwcUrlRecv, 'make_invoice')
-  return await createInvoice({ msats: 1000, expiry: 1 }, { nwcUrlRecv })
+  return await withTimeout(createInvoice({ msats: 1000, expiry: 1 }, { nwcUrlRecv }), 5000)
 }
 
 export async function createInvoice (

dda7e183d92c22fb228ea2161abd1f614c0c045b
ekzyis <ek@stacker.news>
2024-08-19 19:22:38 -0500

    Always use withTimeout in NWC test functions

--- wallets/nwc/client.js
+++ wallets/nwc/client.js
@@ -1,8 +1,11 @@
+import { withTimeout } from '@/lib/time'
 import { hasMethod, nwcCall } from 'wallets/nwc'
 export * from 'wallets/nwc'
 
 export async function testSendPayment ({ nwcUrl }, { logger }) {
-  const supported = await hasMethod(nwcUrl, 'pay_invoice', { logger })
+  const timeout = 15_000
+
+  const supported = await withTimeout(hasMethod(nwcUrl, 'pay_invoice', { logger }), timeout)
   if (!supported) {
     throw new Error('pay_invoice not supported')
   }
--- wallets/nwc/server.js
+++ wallets/nwc/server.js
@@ -3,17 +3,19 @@ import { hasMethod, nwcCall } from 'wallets/nwc'
 export * from 'wallets/nwc'
 
 export async function testCreateInvoice ({ nwcUrlRecv }) {
-  const supported = await hasMethod(nwcUrlRecv, 'make_invoice')
+  const timeout = 15_000
+
+  const supported = await withTimeout(hasMethod(nwcUrlRecv, 'make_invoice'), timeout)
   if (!supported) {
     throw new Error('make_invoice not supported')
   }
 
-  const unrestricted = await hasMethod(nwcUrlRecv, 'pay_invoice')
+  const unrestricted = await withTimeout(hasMethod(nwcUrlRecv, 'pay_invoice'), timeout)
   if (unrestricted) {
     throw new Error('pay_invoice must not be supported')
   }
 
-  return await withTimeout(createInvoice({ msats: 1000, expiry: 1 }, { nwcUrlRecv }), 5000)
+  return await withTimeout(createInvoice({ msats: 1000, expiry: 1 }, { nwcUrlRecv }), timeout)
 }
 
 export async function createInvoice (

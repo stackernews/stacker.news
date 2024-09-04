15eb27b2cb360169fccc75a6d70f1b77d995e830
ekzyis <ek@stacker.news>
2024-09-05 00:42:44 +0200

    Apply multiAuthMiddleware in /api/graphql

--- middleware.js
+++ middleware.js
@@ -69,44 +69,6 @@ function referrerMiddleware (request) {
   return response
 }
 
-// TODO: use this middleware to switch between multiple auth accounts
-function multiAuthMiddleware (request) {
-  // switch next-auth session cookie with multi_auth cookie if cookie pointer present
-
-  // is there a cookie pointer?
-  const cookiePointerName = 'multi_auth.user-id'
-  const hasCookiePointer = request.cookies?.has(cookiePointerName)
-  // is there a session?
-  const sessionCookieName = request.secure ? '__Secure-next-auth.session-token' : 'next-auth.session-token'
-  const hasSession = request.cookies?.has(sessionCookieName)
-
-  if (!hasCookiePointer || !hasSession) {
-    // no session or no cookie pointer. do nothing.
-    return NextResponse.next({ request })
-  }
-
-  const userId = request.cookies?.get(cookiePointerName)?.value
-  if (userId === 'anonymous') {
-    // user switched to anon. only delete session cookie.
-    request.cookies.delete(sessionCookieName)
-    return NextResponse.next({ request })
-  }
-
-  const userJWT = request.cookies.get(`multi_auth.${userId}`)?.value
-  if (!userJWT) {
-    // no multi auth JWT found
-    return NextResponse.next({ request })
-  }
-
-  if (userJWT) {
-    // multi auth JWT found in cookie that pointed to by cookie pointer that is different to current session cookie.
-    request.cookies.set(sessionCookieName, userJWT)
-    return NextResponse.next({ request })
-  }
-
-  return NextResponse.next({ request })
-}
-
 export function middleware (request) {
   const resp = referrerMiddleware(request)
 

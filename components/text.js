f15d9524e813f59b7b31e924baf53e1c3c8646ac
k00b <k00b@stacker.news>
2024-09-28 13:06:34 -0500

    refactor markdown rendering more + better footnotes

--- components/text.js
+++ components/text.js
@@ -3,7 +3,7 @@ import ReactMarkdown from 'react-markdown'
 import gfm from 'remark-gfm'
 import { LightAsync as SyntaxHighlighter } from 'react-syntax-highlighter'
 import atomDark from 'react-syntax-highlighter/dist/cjs/styles/prism/atom-dark'
-import React, { useState, memo, useRef, useCallback, useMemo, useEffect } from 'react'
+import React, { useState, memo, useRef, useCallback, useMemo, useEffect, createElement } from 'react'
 import { slug } from 'github-slugger'
 import LinkIcon from '@/svgs/link.svg'
 import Thumb from '@/svgs/thumb-up-fill.svg'
@@ -59,9 +59,9 @@ export default memo(function Text ({ rel, imgproxyUrls, children, tab, itemId, o
 
   // if we are navigating to a hash, show the full text
   useEffect(() => {
-    setShow(router.asPath.includes('#'))
+    setShow(router.asPath.includes('#') && !router.asPath.includes('#itemfn-'))
     const handleRouteChange = (url, { shallow }) => {
-      setShow(url.includes('#'))
+      setShow(url.includes('#') && !url.includes('#itemfn-'))
     }
 
     router.events.on('hashChangeStart', handleRouteChange)
@@ -69,7 +69,7 @@ export default memo(function Text ({ rel, imgproxyUrls, children, tab, itemId, o
     return () => {
       router.events.off('hashChangeStart', handleRouteChange)
     }
-  }, [router])
+  }, [router.asPath, router.events])
 
   // clip item and give it a`show full text` button if we are overflowing
   useEffect(() => {
@@ -94,117 +94,27 @@ export default memo(function Text ({ rel, imgproxyUrls, children, tab, itemId, o
     }
   }, [containerRef.current, setOverflowing])
 
-  const Heading = useCallback(({ children, node, ...props }) => {
-    const [copied, setCopied] = useState(false)
-    const nodeText = toString(node)
-    const id = useMemo(() => noFragments ? undefined : slug(nodeText.replace(/[^\w\-\s]+/gi, '')), [nodeText, noFragments])
-    const h = useMemo(() => {
-      if (topLevel) {
-        return node?.TagName
-      }
-
-      const h = parseInt(node?.tagName?.replace('h', '') || 0)
-      if (h < 4) return `h${h + 3}`
-
-      return 'h6'
-    }, [node, topLevel])
-    const Icon = copied ? Thumb : LinkIcon
-
-    return (
-      <span className={styles.heading}>
-        {React.createElement(h || node?.tagName, { id, ...props }, children)}
-        {!noFragments && topLevel &&
-          <a className={`${styles.headingLink} ${copied ? styles.copied : ''}`} href={`#${id}`}>
-            <Icon
-              onClick={() => {
-                const location = new URL(window.location)
-                location.hash = `${id}`
-                copy(location.href)
-                setTimeout(() => setCopied(false), 1500)
-                setCopied(true)
-              }}
-              width={18}
-              height={18}
-              className='fill-grey'
-            />
-          </a>}
-      </span>
-    )
-  }, [topLevel, noFragments])
-
-  const Table = useCallback(({ node, ...props }) =>
-    <span className='table-responsive'>
-      <table className='table table-bordered table-sm' {...props} />
-    </span>, [])
-
-  const Code = useCallback(({ node, inline, className, children, style, ...props }) => {
-    return inline
-      ? (
-        <code className={className} {...props}>
-          {children}
-        </code>
-        )
-      : (
-        <SyntaxHighlighter style={atomDark} language='text' PreTag='div' {...props}>
-          {children}
-        </SyntaxHighlighter>
-        )
-  }, [])
-
-  const P = useCallback(({ children, node, onlyImages, somethingBefore, somethingAfter, ...props }) =>
-    <div
-      className={classNames(styles.p, onlyImages && styles.onlyImages,
-        somethingBefore && styles.somethingBefore, somethingAfter && styles.somethingAfter)} {...props}
-    >{children}
-    </div>, [])
-
-  const TextMediaOrLink = useCallback(({ node, src, ...props }) => {
-    const url = IMGPROXY_URL_REGEXP.test(src) ? decodeProxyUrl(src) : src
-    // if outlawed, render the media link as text
-    if (outlawed) {
-      return url
-    }
-    const srcSet = imgproxyUrls?.[url]
-
-    return <MediaOrLink srcSet={srcSet} tab={tab} src={src} rel={rel ?? UNKNOWN_LINK_REL} {...props} topLevel={topLevel} />
-  }, [imgproxyUrls, topLevel, tab, outlawed, rel])
+  const TextMediaOrLink = useCallback(props => {
+    return <MediaLink {...props} outlawed={outlawed} imgproxyUrls={imgproxyUrls} topLevel={topLevel} rel={rel} />
+  },
+  [outlawed, imgproxyUrls, topLevel, rel])
+  const H = useCallback(props => <Heading {...props} topLevel={topLevel} noFragments={noFragments} />,
+    [topLevel, noFragments])
 
   const components = useMemo(() => ({
-    h1: Heading,
-    h2: Heading,
-    h3: Heading,
-    h4: Heading,
-    h5: Heading,
-    h6: Heading,
+    h1: H,
+    h2: H,
+    h3: H,
+    h4: H,
+    h5: H,
+    h6: H,
     table: Table,
     p: P,
-    li: props => {
-      return <li {...props} id={props.id && itemId ? `${props.id}-${itemId}` : props.id} />
-    },
     code: Code,
-    span: ({ children, className, ...props }) => <span className={className}>{children}</span>,
-    mention: ({ children, href, name, id }) => {
-      return (
-        <UserPopover name={name}>
-          <Link
-            id={id}
-            href={href}
-          >
-            {children}
-          </Link>
-        </UserPopover>
-      )
-    },
-    sub: ({ children, href, ...props }) => {
-      return <Link href={href}>{children}</Link>
-    },
-    item: ({ children, href, id }) => {
-      return (
-        <ItemPopover id={id}>
-          <Link href={href}>{children}</Link>
-        </ItemPopover>
-      )
-    },
+    mention: Mention,
+    sub: Sub,
+    item: Item,
+    footnote: Footnote,
     a: ({ node, href, children, ...props }) => {
       // if outlawed, render the link as text
       if (outlawed) {
@@ -214,16 +124,6 @@ export default memo(function Text ({ rel, imgproxyUrls, children, tab, itemId, o
       // if the link has text, and it's not a URL, render it as an external link
       const text = children[0]
       if (!!text && !/^https?:\/\//.test(text)) {
-        if (props['data-footnote-ref'] || typeof props['data-footnote-backref'] !== 'undefined') {
-          return (
-            <Link
-              {...props}
-              id={props.id && itemId ? `${props.id}-${itemId}` : props.id}
-              href={itemId ? `${href}-${itemId}` : href}
-            >{text}
-            </Link>
-          )
-        }
         return (
           // eslint-disable-next-line
           <a id={props.id} target='_blank' rel={rel ?? UNKNOWN_LINK_REL} href={href}>{children}</a>
@@ -231,11 +131,11 @@ export default memo(function Text ({ rel, imgproxyUrls, children, tab, itemId, o
       }
 
       // assume the link is an image which will fallback to link if it's not
-      return <TextMediaOrLink src={href} rel={rel ?? UNKNOWN_LINK_REL} {...props}>{children}</TextMediaOrLink>
+      return <TextMediaOrLink src={href} {...props}>{children}</TextMediaOrLink>
     },
     img: TextMediaOrLink,
     embed: Embed
-  }), [outlawed, rel, itemId, Code, P, Heading, Table, TextMediaOrLink, Embed])
+  }), [outlawed, rel, itemId, H, TextMediaOrLink])
 
   const carousel = useCarousel()
 
@@ -244,10 +144,13 @@ export default memo(function Text ({ rel, imgproxyUrls, children, tab, itemId, o
       components={components}
       remarkPlugins={remarkPlugins}
       rehypePlugins={rehypePlugins}
+      remarkRehypeOptions={{ clobberPrefix: `itemfn-${itemId}-` }}
     >
       {children}
     </ReactMarkdown>
-  ), [components, remarkPlugins, rehypePlugins, children])
+  ), [components, remarkPlugins, rehypePlugins, children, itemId])
+
+  const showOverflow = useCallback(() => setShow(true), [setShow])
 
   return (
     <div
@@ -258,15 +161,17 @@ export default memo(function Text ({ rel, imgproxyUrls, children, tab, itemId, o
       )}
       ref={containerRef}
     >
-      {carousel && tab !== 'preview'
-        ? markdownContent
-        : <CarouselProvider>{markdownContent}</CarouselProvider>}
+      {
+        carousel && tab !== 'preview'
+          ? markdownContent
+          : <CarouselProvider>{markdownContent}</CarouselProvider>
+      }
       {overflowing && !show && (
         <Button
           size='lg'
           variant='info'
           className={styles.textShowFull}
-          onClick={() => setShow(true)}
+          onClick={showOverflow}
         >
           show full text
         </Button>
@@ -274,3 +179,119 @@ export default memo(function Text ({ rel, imgproxyUrls, children, tab, itemId, o
     </div>
   )
 }, isEqual)
+
+function Mention ({ children, href, name, id }) {
+  return (
+    <UserPopover name={name}>
+      <Link
+        id={id}
+        href={href}
+      >
+        {children}
+      </Link>
+    </UserPopover>
+  )
+}
+
+function Sub ({ children, href, ...props }) {
+  return <Link href={href}>{children}</Link>
+}
+
+function Item ({ children, href, id }) {
+  return (
+    <ItemPopover id={id}>
+      <Link href={href}>{children}</Link>
+    </ItemPopover>
+  )
+}
+
+function Footnote ({ children, ...props }) {
+  return (
+    <Link {...props}>{children}</Link>
+  )
+}
+
+function MediaLink ({
+  node, src, outlawed, imgproxyUrls, rel = UNKNOWN_LINK_REL, ...props
+}) {
+  const url = IMGPROXY_URL_REGEXP.test(src) ? decodeProxyUrl(src) : src
+  // if outlawed, render the media link as text
+  if (outlawed) {
+    return url
+  }
+
+  const srcSet = imgproxyUrls?.[url]
+
+  return <MediaOrLink srcSet={srcSet} src={src} rel={rel} {...props} />
+}
+
+function Heading ({ children, node, topLevel, noFragments, ...props }) {
+  const [copied, setCopied] = useState(false)
+  const nodeText = toString(node)
+  const id = useMemo(() => noFragments ? undefined : slug(nodeText.replace(/[^\w\-\s]+/gi, '')), [nodeText, noFragments])
+  const h = useMemo(() => {
+    if (topLevel) {
+      return node?.tagName
+    }
+
+    const h = parseInt(node?.tagName?.replace('h', '') || 0)
+    if (h < 4) return `h${h + 3}`
+
+    return 'h6'
+  }, [node?.tagName, topLevel])
+  const onClick = useCallback(() => {
+    const location = new URL(window.location)
+    location.hash = id
+    copy(location.href)
+    setTimeout(() => setCopied(false), 1500)
+    setCopied(true)
+  }, [id])
+  const Icon = copied ? Thumb : LinkIcon
+
+  return (
+    <span className={styles.heading}>
+      {createElement(h, { id, ...props }, children)}
+      {!noFragments && topLevel &&
+        <a className={classNames(styles.headingLink, copied && styles.copied)} href={`#${id}`} onClick={onClick}>
+          <Icon
+            width={18}
+            height={18}
+            className='fill-grey'
+          />
+        </a>}
+    </span>
+  )
+}
+
+function Table ({ node, ...props }) {
+  return (
+    <span className='table-responsive'>
+      <table className='table table-bordered table-sm' {...props} />
+    </span>
+  )
+}
+
+function Code ({ node, inline, className, children, style, ...props }) {
+  return inline
+    ? (
+      <code className={className} {...props}>
+        {children}
+      </code>
+      )
+    : (
+      <SyntaxHighlighter style={atomDark} language='text' PreTag='div' {...props}>
+        {children}
+      </SyntaxHighlighter>
+      )
+}
+
+function P ({ children, node, onlyImages, somethingBefore, somethingAfter, ...props }) {
+  return (
+    <div
+      className={classNames(styles.p, onlyImages && styles.onlyImages,
+        somethingBefore && styles.somethingBefore, somethingAfter && styles.somethingAfter)} {...props}
+    >
+      {children}
+    </div>
+  )
+}

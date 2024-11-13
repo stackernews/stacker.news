57843a9676d7d50f684d4455191f0a907932ab18
aegroto <aegroto@protonmail.com>
2024-11-13 23:50:52 +0100

    chore: added checkbox on recent header

--- components/recent-header.js
+++ components/recent-header.js
@@ -1,6 +1,7 @@
 import { ITEM_TYPES, ITEM_TYPES_UNIVERSAL } from '@/lib/constants'
-import { Select } from './form'
+import { Checkbox, Select } from './form'
 import { useRouter } from 'next/router'
+import { Formik } from 'formik'
 
 export default function RecentHeader ({ type, sub }) {
   const router = useRouter()
@@ -14,18 +15,34 @@ export default function RecentHeader ({ type, sub }) {
     : ITEM_TYPES
 
   type ||= router.query.type || type || 'posts'
+
+  const activeBountiesCheckbox = (
+    type === 'bounties'
+      ? (
+        <Checkbox label='active only' />
+        )
+      : <div />
+  )
+
   return (
-    <div className='text-muted fw-bold my-1 d-flex justify-content-start align-items-center'>
-      <Select
-        groupClassName='mb-2'
-        className='w-auto'
-        name='type'
-        size='sm'
-        value={type}
-        items={items}
-        noForm
-        onChange={(_, e) => router.push(prefix + (e.target.value === 'posts' ? '/recent' : `/recent/${e.target.value}`))}
-      />
+    <div className='flex-wrap'>
+      <Formik>
+        <div className='text-muted fw-bold my-1 d-flex justify-content-start align-items-center'>
+          <Select
+            groupClassName='mb-2'
+            className='w-auto'
+            name='type'
+            size='sm'
+            value={type}
+            items={items}
+            noForm
+            onChange={(_, e) => router.push(prefix + (e.target.value === 'posts' ? '/recent' : `/recent/${e.target.value}`))}
+          />
+          <div className='mx-2 my-1'>
+            {activeBountiesCheckbox}
+          </div>
+        </div>
+      </Formik>
     </div>
   )
 }

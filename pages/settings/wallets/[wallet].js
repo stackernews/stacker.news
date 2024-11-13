13d64451a0e7777d519af9b647ad11dea908c395
ekzyis <ek@stacker.news>
2024-11-13 22:05:10 +0100

    Also use wallet logo on config page

--- pages/settings/wallets/[wallet].js
+++ pages/settings/wallets/[wallet].js
@@ -67,10 +67,16 @@ export default function WalletSettings () {
     }
   }, [wallet.def])
 
+  const { card: { image, title, subtitle } } = wallet?.def || { card: {} }
+
   return (
     <CenterLayout>
-      <h2 className='pb-2'>{wallet?.def.card.title}</h2>
-      <h6 className='text-muted text-center pb-3'><Text>{wallet?.def.card.subtitle}</Text></h6>
+      {image
+        ? typeof image === 'object'
+          ? <img {...image} alt={title} className='pb-2' />
+          : <img src={image} width='33%' alt={title} className='pb-2' />
+        : <h2 className='pb-2'>{title}</h2>}
+      <h6 className='text-muted text-center pb-3'><Text>{subtitle}</Text></h6>
       <Form
         initial={initial}
         enableReinitialize

b0c1c8768612b03192b2741983f917442e33e392
Felipe Bueno <bueno.felipe@gmail.com>
2024-11-20 11:11:53 -0300

    Introduce SubPopover

---
+++ components/sub-popover.js
@@ -0,0 +1,28 @@
+import { SUB_FULL } from '@/fragments/subs'
+import errorStyles from '@/styles/error.module.css'
+import { useLazyQuery } from '@apollo/client'
+import classNames from 'classnames'
+import HoverablePopover from './hoverable-popover'
+import { TerritoryInfo, TerritoryInfoSkeleton } from './territory-header'
+
+export default function SubPopover ({ sub, children }) {
+  const [getSub, { loading, data }] = useLazyQuery(
+    SUB_FULL,
+    {
+      variables: { sub },
+      fetchPolicy: 'cache-first'
+    }
+  )
+
+  return (
+    <HoverablePopover
+      onShow={getSub}
+      trigger={children}
+      body={!data || loading
+        ? <TerritoryInfoSkeleton />
+        : !data.sub
+            ? <h1 className={classNames(errorStyles.status, errorStyles.describe)}>SUB NOT FOUND</h1>
+            : <TerritoryInfo sub={data.sub} />}
+    />
+  )
+}

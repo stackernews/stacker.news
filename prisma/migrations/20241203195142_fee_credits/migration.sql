89504f0a333877355501ac307674f7e89c92e7a7
k00b <k00b@stacker.news>
2024-12-04 12:19:09 -0600

    squash migrations

--- prisma/migrations/20241203195142_fee_credits/migration.sql
+++ prisma/migrations/20241203195142_fee_credits/migration.sql
@@ -1,10 +1,13 @@
 -- AlterTable
-ALTER TABLE "Item" ADD COLUMN     "mcredits" BIGINT NOT NULL DEFAULT 0;
-
--- AlterTable
-ALTER TABLE "ItemUserAgg" ADD COLUMN     "zapCredits" BIGINT NOT NULL DEFAULT 0;
+ALTER TABLE "Item" ADD COLUMN     "mcredits" BIGINT NOT NULL DEFAULT 0,
+ADD COLUMN     "commentMcredits" BIGINT NOT NULL DEFAULT 0;
 
 -- AlterTable
 ALTER TABLE "users" ADD COLUMN     "mcredits" BIGINT NOT NULL DEFAULT 0,
+ADD COLUMN     "stackedMcredits" BIGINT NOT NULL DEFAULT 0,
 ADD COLUMN     "receiveCreditsBelowSats" INTEGER NOT NULL DEFAULT 10,
 ADD COLUMN     "sendCreditsBelowSats" INTEGER NOT NULL DEFAULT 10;
+
+-- add mcredits check
+ALTER TABLE users ADD CONSTRAINT "mcredits_positive" CHECK ("mcredits" >= 0) NOT VALID;
+ALTER TABLE users ADD CONSTRAINT "stackedMcredits_positive" CHECK ("stackedMcredits" >= 0) NOT VALID;

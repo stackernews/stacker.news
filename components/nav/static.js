52098a3e5011cb405a71b32c84e098f8c603d484
k00b <k00b@stacker.news>
2024-12-09 19:03:30 -0600

    fix broken static header from carousel

--- components/nav/static.js
+++ components/nav/static.js
@@ -1,19 +1,22 @@
 import { Container, Nav, Navbar } from 'react-bootstrap'
 import styles from '../header.module.css'
 import { BackOrBrand, NavPrice, SearchItem } from './common'
+import { CarouselProvider } from './carousel'
 
 export default function StaticHeader () {
   return (
-    <Container as='header' className='px-sm-0'>
-      <Navbar>
-        <Nav
-          className={styles.navbarNav}
-        >
-          <BackOrBrand />
-          <SearchItem />
-          <NavPrice className='justify-content-end' />
-        </Nav>
-      </Navbar>
-    </Container>
+    <CarouselProvider>
+      <Container as='header' className='px-sm-0'>
+        <Navbar>
+          <Nav
+            className={styles.navbarNav}
+          >
+            <BackOrBrand />
+            <SearchItem />
+            <NavPrice className='justify-content-end' />
+          </Nav>
+        </Navbar>
+      </Container>
+    </CarouselProvider>
   )
 }

c076b230188e2625b81352c709db37925284e863
k00b <k00b@stacker.news>
2024-12-10 18:19:56 -0600

    restore migration from master

--- prisma/migrations/20241203235457_invite_denormalized_count/migration.sql
+++ prisma/migrations/20241203235457_invite_denormalized_count/migration.sql
@@ -5,3 +5,4 @@ ALTER TABLE "Invite" ADD COLUMN "giftedCount" INTEGER NOT NULL DEFAULT 0;
 UPDATE "Invite"
 SET "giftedCount" = (SELECT COUNT(*) FROM "users" WHERE "users"."inviteId" = "Invite".id)
 WHERE "Invite"."id" = "Invite".id;
+

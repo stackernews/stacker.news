8fbf5c25ec25c11ccd413c941666b011621691e6
Riccardo Balbo <riccardo0blb@gmail.com>
2024-12-17 20:43:06 +0100

    Add create invoice test

--- wallets/bolt12/server.js
+++ wallets/bolt12/server.js
@@ -1,9 +1,20 @@
 import { withTimeout } from '@/lib/time'
+import lnd from '@/api/lnd'
+import { fetchBolt12InvoiceFromOffer } from '@/lib/lndk'
+import { isBolt12Invoice } from '@/lib/bolt12'
+import { parseInvoice } from '@/lib/boltInvoices'
+import { toPositiveNumber } from '@/lib/format'
 export * from '@/wallets/bolt12'
 
 export async function testCreateInvoice ({ offer }) {
   const timeout = 15_000
-  return await withTimeout(createInvoice({ msats: 1000, expiry: 1 }, { offer }), timeout)
+  return await withTimeout((async () => {
+    const invoice = await fetchBolt12InvoiceFromOffer({ lnd, offer, msats: 1000, description: 'test' })
+    if (!isBolt12Invoice(invoice)) throw new Error('not a bolt12 invoice')
+    const parsedInvoice = await parseInvoice({ lnd, request: invoice })
+    if (toPositiveNumber(parsedInvoice.mtokens) !== 1000) throw new Error('invalid invoice response')
+    return offer
+  })(), timeout)
 }
 
 export async function createInvoice ({ msats, description, expiry }, { offer }) {

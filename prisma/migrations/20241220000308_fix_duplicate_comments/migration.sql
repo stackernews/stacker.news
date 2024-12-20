a4b4a6a247cac319ec7380146506682bbcf7de99
ekzyis <ek@stacker.news>
2024-12-20 19:55:22 +0100

    Use MD5 for slightly better performance

--- prisma/migrations/20241220000308_fix_duplicate_comments/migration.sql
+++ prisma/migrations/20241220000308_fix_duplicate_comments/migration.sql
@@ -6,10 +6,10 @@ ALTER TABLE "Item" ADD CONSTRAINT "Item_unique_time_constraint"
     -- we use COALESCE so NULL is considered equal to itself
     COALESCE("parentId", -1) WITH =,
     COALESCE("title", '') WITH =,
-    -- GiST does not support citext so we cast to bytea
-    digest(COALESCE("subName", ''), 'sha256') WITH =,
-    -- including text column directly can make index row too large so we use the hash
-    digest("text", 'sha256') WITH =,
+    -- GiST does not support citext so we use md5 hash
+    md5(COALESCE("subName", '')) WITH =,
+    -- including text column directly can make index row too large so we use md5 hash
+    md5("text") WITH =,
     tsrange(created_at, created_at + INTERVAL '10 minutes') WITH &&
   )
   -- enforce constraint after this date

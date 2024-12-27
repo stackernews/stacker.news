e93dc91a38efcb2fe179c233ba77c8b3ab87324b
Riccardo Balbo <riccardo0blb@gmail.com>
2024-12-27 11:12:44 +0100

    remove duplicate checks

--- wallets/bolt12/server.js
+++ wallets/bolt12/server.js
@@ -1,12 +1,10 @@
 import { fetchBolt12InvoiceFromOffer } from '@/lib/lndk'
-import { isBolt12Invoice } from '@/lib/bolt12'
 import { parseInvoice } from '@/lib/boltInvoices'
 import { toPositiveNumber } from '@/lib/format'
 export * from '@/wallets/bolt12'
 
 export async function testCreateInvoice ({ offer }, { lnd }) {
   const invoice = await fetchBolt12InvoiceFromOffer({ lnd, offer, msats: 1000, description: 'test' })
-  if (!isBolt12Invoice(invoice)) throw new Error('not a bolt12 invoice')
   const parsedInvoice = await parseInvoice({ lnd, request: invoice })
   if (toPositiveNumber(parsedInvoice.mtokens) !== 1000) throw new Error('invalid invoice response')
   return offer

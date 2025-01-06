5034104a93f96ab920f902fcd3411fd3c941f494
ekzyis <ek@stacker.news>
2025-01-06 22:31:33 +0100

    Fix job edits

--- api/resolvers/item.js
+++ api/resolvers/item.js
@@ -1434,7 +1434,7 @@ export const updateItem = async (parent, { sub: subName, forward, hash, hmac, ..
   // but forever if an admin is editing an "admin item", it's their bio or a job
   const myBio = user.bioId === old.id
   const timer = Date.now() < datePivot(new Date(old.invoicePaidAt ?? old.createdAt), { seconds: ITEM_EDIT_SECONDS })
-  const canEdit = (timer && ownerEdit) || adminEdit || myBio || isJob(item)
+  const canEdit = (timer && ownerEdit) || adminEdit || myBio || isJob(old)
   if (!canEdit) {
     throw new GqlInputError('item can no longer be edited')
   }

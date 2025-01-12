4520c9117943ebe3370b621639eae4a9b0efe42c
Keyan <34140557+huumn@users.noreply.github.com>
2025-01-12 15:15:53 -0600

    cowboy credit modal explainers wherever they are referenced (#1815)

---
+++ components/info/cc.js
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import Info from '.'
+
+export default function CCInfo (props) {
+  return (
+    <Info {...props}>
+      <h6>Why am I getting cowboy credits?</h6>
+      <ul className='line-height-md'>
+        <li>to receive sats, you must attach an <Link href='/wallets'>external receiving wallet</Link></li>
+        <li>zappers may have chosen to send you CCs instead of sats</li>
+        <li>if the zaps are split on post, recepients will receive CCs regardless of their configured receiving wallet</li>
+        <li>there could be an issue paying your receiving wallet
+          <ul>
+            <li>check your <Link href='/wallets/logs'>wallet logs</Link> for clues</li>
+            <li>if you have questions about the errors in your wallet logs, mention the error in the <Link href='/daily'>saloon</Link></li>
+          </ul>
+        </li>
+        <li>some zaps might be smaller than your configured receiving dust limit
+          <ul>
+            <li>you can configure your dust limit in your <Link href='/settings'>settings</Link></li>
+          </ul>
+        </li>
+      </ul>
+    </Info>
+  )
+}

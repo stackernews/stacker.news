384c11b7065ac090c614695f21b90a8f23901110
k00b <k00b@stacker.news>
2025-01-29 16:23:59 -0600

    squash migrations

--- prisma/migrations/20250118010433_comment_pages/migration.sql
+++ prisma/migrations/20250118010433_comment_pages/migration.sql
@@ -1,3 +1,17 @@
+-- AlterTable
+ALTER TABLE "Item" ADD COLUMN "nDirectComments" INTEGER NOT NULL DEFAULT 0;
+
+-- Update nDirectComments
+UPDATE "Item"
+SET "nDirectComments" = "DirectComments"."nDirectComments"
+FROM (
+    SELECT "Item"."parentId" AS "id", COUNT(*) AS "nDirectComments"
+    FROM "Item"
+    WHERE "Item"."parentId" IS NOT NULL
+    GROUP BY "Item"."parentId"
+) AS "DirectComments"
+WHERE "Item"."id" = "DirectComments"."id";
+
 -- add limit and offset
 CREATE OR REPLACE FUNCTION item_comments_zaprank_with_me_limited(
     _item_id int, _global_seed int, _me_id int, _limit int, _offset int, _grandchild_limit int,

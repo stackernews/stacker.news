88da60a2ab1397e17dbe225cb4383c73d43d39b9
ekzyis <ek@stacker.news>
2025-03-19 20:52:47 -0500

    Truncate wallet logs

--- prisma/migrations/20250118192921_normalized_wallet_logs/migration.sql
+++ prisma/migrations/20250118192921_normalized_wallet_logs/migration.sql
@@ -4,3 +4,5 @@ ALTER TABLE "WalletLog"
 
 ALTER TABLE "WalletLog" ADD CONSTRAINT "WalletLog_invoiceId_fkey" FOREIGN KEY ("invoiceId") REFERENCES "Invoice"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 ALTER TABLE "WalletLog" ADD CONSTRAINT "WalletLog_withdrawalId_fkey" FOREIGN KEY ("withdrawalId") REFERENCES "Withdrawl"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+TRUNCATE "WalletLog" RESTRICT;

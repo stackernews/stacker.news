83befcceb78a3ef9a93f9b5837ab369ad14fa478
ekzyis <ek@stacker.news>
2025-03-26 11:30:09 -0500

    Add wallet indicator

--- components/nav/mobile/offcanvas.js
+++ components/nav/mobile/offcanvas.js
@@ -7,6 +7,7 @@ import AnonIcon from '@/svgs/spy-fill.svg'
 import styles from './footer.module.css'
 import canvasStyles from './offcanvas.module.css'
 import classNames from 'classnames'
+import { useWalletIndicator } from '@/components/wallet-indicator'
 
 export default function OffCanvas ({ me, dropNavKey }) {
   const [show, setShow] = useState(false)
@@ -26,6 +27,7 @@ export default function OffCanvas ({ me, dropNavKey }) {
     : <span className='text-muted pointer'><AnonIcon onClick={onClick} width='22' height='22' /></span>
 
   const profileIndicator = me && !me.bioId
+  const walletIndicator = useWalletIndicator()
 
   return (
     <>
@@ -59,7 +61,10 @@ export default function OffCanvas ({ me, dropNavKey }) {
                     <Dropdown.Item active={me.name + '/bookmarks' === dropNavKey}>bookmarks</Dropdown.Item>
                   </Link>
                   <Link href='/wallets' passHref legacyBehavior>
-                    <Dropdown.Item eventKey='wallets'>wallets</Dropdown.Item>
+                    <Dropdown.Item eventKey='wallets'>
+                      wallets
+                      {walletIndicator && <Indicator />}
+                    </Dropdown.Item>
                   </Link>
                   <Link href='/credits' passHref legacyBehavior>
                     <Dropdown.Item eventKey='credits'>credits</Dropdown.Item>

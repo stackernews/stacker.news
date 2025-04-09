0fee52381a4a8ddba72d9bd18d036609fc136666
ekzyis <ek@stacker.news>
2025-04-09 18:54:35 +0200

    Add user migration

--- prisma/migrations/20250328203730_wallet_badges/migration.sql
+++ prisma/migrations/20250328203730_wallet_badges/migration.sql
@@ -23,3 +23,27 @@ $$ LANGUAGE plpgsql;
 CREATE OR REPLACE TRIGGER check_wallet_trigger
 AFTER INSERT OR UPDATE OR DELETE ON "Wallet"
 FOR EACH ROW EXECUTE PROCEDURE check_wallet_trigger();
+
+CREATE OR REPLACE FUNCTION wallet_badges_user_migration()
+RETURNS INTEGER
+LANGUAGE plpgsql
+AS $$
+DECLARE
+BEGIN
+    INSERT INTO pgboss.job (name, data, retrylimit, startafter, keepuntil)
+    SELECT
+        'checkWallet',
+        jsonb_build_object('userId', "users"."id"),
+        -- XXX we have around 1000 users with wallets
+        -- to balance the db and network load (push notifications) a little bit,
+        -- we select a random start time between now and 5 minutes.
+        21, now() + (random() * interval '5 minutes'), now() + interval '10 minutes'
+    FROM "users"
+    WHERE EXISTS ( SELECT 1 FROM "Wallet" WHERE "users"."id" = "Wallet"."userId" );
+    return 0;
+EXCEPTION WHEN OTHERS THEN
+    return 0;
+END;
+$$;
+
+DROP FUNCTION IF EXISTS wallet_badges_user_migration;

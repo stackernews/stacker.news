bbd4fa9ec5764e0352eda559c9f25d134e004c04
ekzyis <ek@stacker.news>
2025-05-17 21:59:45 -0500

    Migrate vault entries to new schema (#2092)
    * Migrate existing vault entries to new schema
    
    * Read+write new vault schema
    
    * Drop VaultEntry table
    
    * Refactor vaultPrismaFragments
    
    * Remove wrong comment
    
    * Remove TODO
    
    * Fix possible race condition on update of vault key
    
    * Remove lib/object.js

--- api/resolvers/vault.js
+++ api/resolvers/vault.js
@@ -1,11 +1,23 @@
 import { E_VAULT_KEY_EXISTS, GqlAuthenticationError, GqlInputError } from '@/lib/error'
+import { getWalletByType } from '@/wallets/common'
+import { deleteVault, hasVault, vaultNewSchematoTypedef, vaultPrismaFragments } from '@/wallets/vault'
 
 export default {
   Query: {
     getVaultEntries: async (parent, args, { me, models }) => {
       if (!me) throw new GqlAuthenticationError()
 
-      return await models.vaultEntry.findMany({ where: { userId: me.id } })
+      const wallets = await models.wallet.findMany({
+        where: { userId: me.id },
+        include: vaultPrismaFragments.include()
+      })
+
+      const vaultEntries = []
+      for (const wallet of wallets) {
+        vaultEntries.push(...vaultNewSchematoTypedef(wallet).vaultEntries)
+      }
+
+      return vaultEntries
     }
   },
   Mutation: {
@@ -13,30 +25,37 @@ export default {
     updateVaultKey: async (parent, { entries, hash }, { me, models }) => {
       if (!me) throw new GqlAuthenticationError()
       if (!hash) throw new GqlInputError('hash required')
-      const txs = []
 
       const { vaultKeyHash: oldKeyHash } = await models.user.findUnique({ where: { id: me.id } })
       if (oldKeyHash) {
-        if (oldKeyHash !== hash) {
-          throw new GqlInputError('vault key already set', E_VAULT_KEY_EXISTS)
-        } else {
+        if (oldKeyHash === hash) {
           return true
         }
-      } else {
-        txs.push(models.user.update({
-          where: { id: me.id },
-          data: { vaultKeyHash: hash }
-        }))
+        throw new GqlInputError('vault key already set', E_VAULT_KEY_EXISTS)
       }
 
-      for (const entry of entries) {
-        txs.push(models.vaultEntry.update({
-          where: { userId_key: { userId: me.id, key: entry.key } },
-          data: { value: entry.value, iv: entry.iv }
-        }))
-      }
-      await models.$transaction(txs)
-      return true
+      return await models.$transaction(async tx => {
+        const wallets = await tx.wallet.findMany({ where: { userId: me.id } })
+        for (const wallet of wallets) {
+          const def = getWalletByType(wallet.type)
+          await tx.wallet.update({
+            where: { id: wallet.id },
+            data: {
+              [def.walletField]: {
+                update: vaultPrismaFragments.upsert({ ...wallet, vaultEntries: entries })
+              }
+            }
+          })
+        }
+
+        // optimistic concurrency control: make sure the user's vault key didn't change while we were updating the wallets
+        await tx.user.update({
+          where: { id: me.id, vaultKeyHash: oldKeyHash },
+          data: { vaultKeyHash: hash }
+        })
+
+        return true
+      })
     },
     clearVault: async (parent, args, { me, models }) => {
       if (!me) throw new GqlAuthenticationError()
@@ -45,7 +64,10 @@ export default {
         where: { id: me.id },
         data: { vaultKeyHash: '' }
       }))
-      txs.push(models.vaultEntry.deleteMany({ where: { userId: me.id } }))
+
+      const wallets = await models.wallet.findMany({ where: { userId: me.id } })
+      txs.push(...wallets.filter(hasVault).map(wallet => deleteVault(models, wallet)))
+
       await models.$transaction(txs)
       return true
     }

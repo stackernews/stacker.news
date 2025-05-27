8fb1748d3239fb9745dedfe2f83169088df2d146
ekzyis <ek@stacker.news>
2025-05-27 01:11:24 -0500

    Use wallet cards again
    My wallet list was quite ugly and I couldn't look at it anymore.

---
+++ wallets/client/components/card.js
@@ -0,0 +1,48 @@
+import { Card } from 'react-bootstrap'
+import styles from '@/styles/wallet.module.css'
+import Plug from '@/svgs/plug.svg'
+import Gear from '@/svgs/settings-5-fill.svg'
+import Link from 'next/link'
+import RecvIcon from '@/svgs/arrow-left-down-line.svg'
+import SendIcon from '@/svgs/arrow-right-up-line.svg'
+import { useWalletImage, useWalletIsConfigured, useWalletSupport, useWalletStatus, WalletStatus } from '@/wallets/client/hooks'
+import { urlify, walletDisplayName } from '@/wallets/client/util'
+
+export default function WalletCard ({ wallet }) {
+  const image = useWalletImage(wallet.name)
+  const status = useWalletStatus(wallet)
+  const support = useWalletSupport(wallet)
+  const isConfigured = useWalletIsConfigured(wallet)
+
+  return (
+    <Card className={styles.card}>
+      <div className={styles.indicators}>
+        {support.receive && <RecvIcon className={`${styles.indicator} ${statusToClass(status.receive)}`} />}
+        {support.send && <SendIcon className={`${styles.indicator} ${statusToClass(status.send)}`} />}
+      </div>
+      <Card.Body>
+        <div className='d-flex text-center align-items-center h-100'>
+          {image
+            ? <img className={styles.walletLogo} {...image} />
+            : <Card.Title className={styles.walletLogo}>{walletDisplayName(wallet.name)}</Card.Title>}
+        </div>
+      </Card.Body>
+      <Link href={`/wallets/${urlify(wallet.name)}`}>
+        <Card.Footer className={styles.attach}>
+          {isConfigured
+            ? <>configure<Gear width={14} height={14} /></>
+            : <>attach<Plug width={14} height={14} /></>}
+        </Card.Footer>
+      </Link>
+    </Card>
+  )
+}
+
+function statusToClass (status) {
+  switch (status) {
+    case WalletStatus.Enabled: return styles.success
+    case WalletStatus.Disabled: return styles.disabled
+    case WalletStatus.Error: return styles.error
+    case WalletStatus.Warning: return styles.warning
+  }
+}

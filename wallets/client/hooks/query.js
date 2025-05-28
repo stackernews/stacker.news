88097a6464f6be7343611f930f047901fe08f32b
ekzyis <ek@stacker.news>
2025-05-28 00:13:31 -0500

    Decrypt wallets
    * useWalletQuery now returns decrypted wallets
    * Refactor useIndexedDB because its only purpose will be to store the key, so no need for pagination code etc.
    * There is still a bug: if the wallet is not decrypted on first render, the form will not see the decrypted value. See TODO.

--- wallets/client/hooks/query.js
+++ wallets/client/hooks/query.js
@@ -1,51 +1,71 @@
 import { WALLET } from '@/fragments/wallet'
 import { useQuery } from '@apollo/client'
-import { useDecryption } from './crypto'
-import { useCallback } from 'react'
+import { useDecryption } from '@/wallets/client/hooks'
+import { useCallback, useEffect, useState } from 'react'
 
 export function useWalletQuery ({ id, name }) {
-  // const decrypt = useWalletDecryption()
   const query = useQuery(WALLET, { variables: { id, name } })
-  const decryptValue = useDecryption()
+  const [wallet, setWallet] = useState(null)
 
-  const decryptConfig = useCallback((config) => {
-    return Object.fromEntries(
-      Object.entries(config)
-        .map(([key, value]) => {
-          const encrypted = value.__typename === 'VaultEntry'
-          if (!encrypted) return [key, value]
+  const decryptWallet = useWalletDecryption()
 
-          // undo the field aliases we had to use because of https://github.com/graphql/graphql-js/issues/53
-          // so we can pretend the GraphQL API returns the fields as they are named in the schema
-          let renamed = key.replace(/^encrypted/, '')
-          renamed = renamed.charAt(0).toLowerCase() + renamed.slice(1)
-          return [
-            renamed,
-            decryptValue(value)
-          ]
-        })
-    )
-  }, [decryptValue])
+  useEffect(() => {
+    if (!query.data?.wallet) return
+    decryptWallet(query.data?.wallet).then(wallet => setWallet(wallet))
+  }, [query.data, decryptWallet])
 
-  if (query.data?.wallet) {
-    const { wallet } = query.data
-    if (wallet.__typename !== 'UserWallet') return query
-
-    const protocols = wallet.protocols.map(protocol => ({
-      ...protocol,
-      config: decryptConfig(protocol.config)
-    }))
-
-    return {
-      ...query,
-      data: {
-        wallet: {
-          ...wallet,
-          protocols
-        }
-      }
-    }
+  return {
+    ...query,
+    // pretend query is still loading until we've decrypted the wallet
+    loading: !!wallet,
+    data: wallet ? { wallet } : null
   }
+}
 
-  return query
+export function useWalletMutation () {
+  // TODO(wallet-v2): implement this. this should encrypt the wallet before sending it to the server
+}
+
+function useWalletDecryption () {
+  const decryptConfig = useDecryptConfig()
+
+  return useCallback(async wallet => {
+    const protocols = await Promise.all(
+      wallet.protocols.map(
+        async protocol => ({
+          ...protocol,
+          config: await decryptConfig(protocol.config)
+        })
+      )
+    )
+    return { ...wallet, protocols }
+  }, [decryptConfig])
+}
+
+function useDecryptConfig () {
+  const decrypt = useDecryption()
+
+  return useCallback(async (config) => {
+    return Object.fromEntries(
+      await Promise.all(
+        Object.entries(config)
+          .map(
+            async ([key, value]) => {
+              const encrypted = value.__typename === 'VaultEntry'
+              if (!encrypted) return [key, value]
+
+              // undo the field aliases we had to use because of https://github.com/graphql/graphql-js/issues/53
+              // so we can pretend the GraphQL API returns the fields as they are named in the schema
+              let renamed = key.replace(/^encrypted/, '')
+              renamed = renamed.charAt(0).toLowerCase() + renamed.slice(1)
+
+              return [
+                renamed,
+                await decrypt(value)
+              ]
+            }
+          )
+      )
+    )
+  }, [decrypt])
 }

20df4432e8146b22cc3ea7e28b066f6cf6415b47
ekzyis <ek@stacker.news>
2025-05-31 23:51:55 -0500

    Run all shared code in generic wallet update function

--- wallets/server/resolvers/protocol.js
+++ wallets/server/resolvers/protocol.js
@@ -4,126 +4,95 @@ import { mapUserWalletResolveTypes } from '@/wallets/server/resolvers/util'
 
 export const resolvers = {
   Mutation: {
-    upsertWalletSendLNbits,
-    upsertWalletRecvLNbits
+    upsertWalletSendLNbits: upsertWalletProtocol({ name: 'LNBITS', send: true }),
+    upsertWalletRecvLNbits: upsertWalletProtocol({ name: 'LNBITS', send: false })
   }
 }
 
-// TODO(wallet-v2): generate these resolvers
-async function upsertWalletSendLNbits (parent, { walletId, templateId, url, apiKey }, { me, models }) {
-  // TODO(wallet-v2): validate and test with HOLD invoice
+function upsertWalletProtocol (protocol) {
+  return async (parent, { walletId, templateId, ...args }, { me, models }) => {
+    if (!me) {
+      throw new GqlAuthenticationError()
+    }
 
-  if (!me) {
-    throw new GqlAuthenticationError()
-  }
+    if (!walletId && !templateId) {
+      throw new GqlInputError('walletId or templateId is required')
+    }
 
-  if (!walletId && !templateId) {
-    throw new GqlInputError('walletId or templateId is required')
-  }
+    const relation = protocolRelationName(protocol)
 
-  if (walletId) {
-    return await updateWallet({
-      walletId: Number(walletId),
-      protocol: { name: 'LNBITS', send: true },
-      data: { url, apiKey }
-    }, { me, models })
-  }
-
-  // TODO(wallet-v2): create new wallet from template
-}
-
-async function upsertWalletRecvLNbits (parent, { walletId, templateId, url, apiKey }, { me, models }) {
-  // TODO(wallet-v2): validate and test with HOLD invoice
-
-  if (!me) {
-    throw new GqlAuthenticationError()
-  }
-
-  if (!walletId && !templateId) {
-    throw new GqlInputError('walletId or templateId is required')
-  }
-
-  if (walletId) {
-    return await updateWallet({
-      walletId: Number(walletId),
-      protocol: { name: 'LNBITS', send: false },
-      data: { url, apiKey }
-    }, { me, models })
-  }
-
-  // TODO(wallet-v2): create new wallet from template
-}
-
-async function updateWallet ({ walletId, protocol, data }, { me, models }) {
-  const relation = protocolRelationName(protocol)
-
-  function toFragment (data) {
-    return Object.fromEntries(
-      Object.entries(data).map(
-        ([key, value]) => {
-          if (isEncryptedField(protocol, key)) {
-            return [key, { update: { value: value.value, iv: value.iv } }]
+    function toFragment (args) {
+      return Object.fromEntries(
+        Object.entries(args).map(
+          ([key, value]) => {
+            if (isEncryptedField(protocol, key)) {
+              return [key, { update: { value: value.value, iv: value.iv } }]
+            }
+            return [key, value]
           }
-          return [key, value]
-        }
+        )
       )
-    )
-  }
+    }
 
-  const dataFragment = toFragment(data)
+    const dataFragment = toFragment(args)
 
-  const [userWallet] = await models.$transaction([
-    models.userWallet.update({
-      where: {
-        id: Number(walletId),
-        // this makes sure that users can only update their own wallets
-        // (the update will fail in this case and abort the transaction)
-        userId: me.id
-      },
-      data: {
-        protocols: {
-          update: {
-            where: {
-              ProtocolWallet_walletId_send_protocol_key: {
-                walletId,
-                send: protocol.send,
-                protocol: protocol.name
+    if (walletId) {
+      const [userWallet] = await models.$transaction([
+        models.userWallet.update({
+          where: {
+            id: Number(walletId),
+            // this makes sure that users can only update their own wallets
+            // (the update will fail in this case and abort the transaction)
+            userId: me.id
+          },
+          data: {
+            protocols: {
+              update: {
+                where: {
+                  ProtocolWallet_walletId_send_protocol_key: {
+                    walletId: Number(walletId),
+                    send: protocol.send,
+                    protocol: protocol.name
+                  }
+                },
+                data: {
+                  [relation]: {
+                    update: dataFragment
+                  }
+                }
               }
-            },
-            data: {
-              [relation]: {
-                update: dataFragment
+            }
+          },
+          include: {
+            protocols: true
+          }
+        }),
+        // XXX Prisma seems to run the vault update AFTER the update of the table that points to it
+        //   which means our trigger to set the jsonb column in the ProtocolWallet table does not see
+        //   the updated vault entry.
+        //   To fix this, we run a protocol wallet update to force the trigger to run again.
+        // TODO(wallet-v2): fix this in a better way?
+        models.protocolWallet.update({
+          where: {
+            ProtocolWallet_walletId_send_protocol_key: {
+              walletId: Number(walletId),
+              send: protocol.send,
+              protocol: protocol.name
+            }
+          },
+          data: {
+            [relation]: {
+              update: {
+                updatedAt: new Date()
               }
             }
           }
-        }
-      },
-      include: {
-        protocols: true
-      }
-    }),
-    // XXX Prisma seems to run the vault update AFTER the update of the table that points to it
-    //   which means our trigger to set the jsonb column in the ProtocolWallet table does not see
-    //   the updated vault entry.
-    //   To fix this, we run a protocol wallet update to force the trigger to run again.
-    // TODO(wallet-v2): fix this in a better way?
-    models.protocolWallet.update({
-      where: {
-        ProtocolWallet_walletId_send_protocol_key: {
-          walletId,
-          send: protocol.send,
-          protocol: protocol.name
-        }
-      },
-      data: {
-        [relation]: {
-          update: {
-            updatedAt: new Date()
-          }
-        }
-      }
-    })
-  ])
+        })
+      ])
 
-  return mapUserWalletResolveTypes(userWallet)
+      return mapUserWalletResolveTypes(userWallet)
+    }
+
+    // TODO(wallet-v2): create new wallet from template
+  }
 }

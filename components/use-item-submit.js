09fe075b084b1704054066141f5f27e1141bb19f
Axel Vyrn <ghoshgairik2020@gmail.com>
2025-06-10 00:22:18 +0530

    Update use-item-submit.js

--- components/use-item-submit.js
+++ components/use-item-submit.js
@@ -87,7 +87,8 @@ export default function useItemSubmit (mutation,
       const response = Object.values(data)[0]
       const postId = response?.result?.id
 
-      if (crosspost && postId) {
+      if (crosspost && postId && !item?.id) {
+        // Only crosspost to Nostr if this is a new post (not editing)
         await crossposter(postId)
       }
 

4ce7c564200f6f6230599863ebdf77601438223b
m0wer <m0wer@autistici.org>
2025-06-10 18:20:49 +0200

    fixup! feat: user deletion functionality

--- components/item-info.js
+++ components/item-info.js
@@ -134,11 +134,13 @@ export default function ItemInfo ({
         {showUser &&
           (item.user.deletedAt
             ? <span className='text-muted'>[deleted]</span>
-            : <Link href={`/${item.user.name}`}>
-              <UserPopover name={item.user.name}>@{item.user.name}</UserPopover>
-              <Badges badgeClassName='fill-grey' spacingClassName='ms-xs' height={12} width={12} user={item.user} />
-              {embellishUser}
+            : (
+              <Link href={`/${item.user.name}`}>
+                <UserPopover name={item.user.name}>@{item.user.name}</UserPopover>
+                <Badges badgeClassName='fill-grey' spacingClassName='ms-xs' height={12} width={12} user={item.user} />
+                {embellishUser}
               </Link>
+              )
           )}
         <span> </span>
         <Link href={`/items/${item.id}`} title={item.invoicePaidAt || item.createdAt} className='text-reset' suppressHydrationWarning>

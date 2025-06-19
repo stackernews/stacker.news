e5563a04983b449d3ac97c727c4d21136500454d
ed-kung <edward.kung@gmail.com>
2025-06-19 11:29:28 -0700

    Revert "fix order for item-full"
    This reverts commit 489e25ea82056bd83a818e581eb2bbfcf947e401.

--- components/text.js
+++ components/text.js
@@ -108,17 +108,9 @@ export default memo(function Text ({ rel = UNKNOWN_LINK_REL, imgproxyUrls, child
   }, [containerRef.current, setOverflowing])
 
   const TextMediaOrLink = useCallback(props => {
-    return (
-      <MediaLink
-        {...props}
-        outlawed={outlawed}
-        imgproxyUrls={imgproxyUrls}
-        topLevel={topLevel}
-        rel={rel}
-        itemId={itemId}
-      />
-    )
-  }, [outlawed, imgproxyUrls, topLevel, rel, itemId])
+    return <MediaLink {...props} outlawed={outlawed} imgproxyUrls={imgproxyUrls} topLevel={topLevel} rel={rel} />
+  },
+  [outlawed, imgproxyUrls, topLevel, rel])
 
   const components = useMemo(() => ({
     h1: ({ node, id, ...props }) => <h1 id={topLevel ? id : undefined} {...props} />,
@@ -228,11 +220,9 @@ function Footnote ({ children, node, ...props }) {
 }
 
 function MediaLink ({
-  node, src, outlawed, imgproxyUrls, itemId, imgIndex, rel = UNKNOWN_LINK_REL, ...props
+  node, src, outlawed, imgproxyUrls, rel = UNKNOWN_LINK_REL, ...props
 }) {
-  // assumes less than 100 images in an item
-  // if more than 100 images, carousel sort order will be unpredictable
-  const globalImgIndex = itemId * 100 + imgIndex
+  const imgIndex = node?.properties?.imgIndex
 
   const url = IMGPROXY_URL_REGEXP.test(src) ? decodeProxyUrl(src) : src
   // if outlawed, render the media link as text
@@ -242,7 +232,7 @@ function MediaLink ({
 
   const srcSet = imgproxyUrls?.[url]
 
-  return <MediaOrLink srcSet={srcSet} src={src} rel={rel} imgIndex={globalImgIndex} {...props} />
+  return <MediaOrLink srcSet={srcSet} src={src} rel={rel} imgIndex={imgIndex} {...props} />
 }
 
 function Table ({ node, ...props }) {

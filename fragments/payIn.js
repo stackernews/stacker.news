e1939d2a9158bec221d5d57c30f43c694120fcaf
k00b <k00b@stacker.news>
2025-07-03 21:47:25 -0500

    wip

--- fragments/payIn.js
+++ fragments/payIn.js
@@ -1,6 +1,11 @@
 import gql from 'graphql-tag'
+import { SUB_FULL_FIELDS } from './subs'
+
+const HASH_HMAC_INPUT_1 = '$hash: String, $hmac: String'
+const HASH_HMAC_INPUT_2 = 'hash: $hash, hmac: $hmac'
 
 export const PAY_IN_FIELDS = gql`
+  ${SUB_FULL_FIELDS}
   fragment PayInFields on PayIn {
     id
     createdAt
@@ -26,10 +31,15 @@ export const PAY_IN_FIELDS = gql`
     result {
       __typename
       ... on Item {
-        ...ItemFields
+        id
+        deleteScheduledAt
+        reminderScheduledAt
       }
       ... on ItemActResult {
-        ...ItemActResultFields
+        id
+        sats
+        path
+        act
       }
       ... on PollVoteResult {
         id
@@ -64,3 +74,145 @@ export const RETRY_PAY_IN = gql`
     }
   }
 `
+
+export const DONATE = gql`
+  ${PAY_IN_FIELDS}
+  mutation donateToRewards($sats: Int!) {
+    donateToRewards(sats: $sats) {
+      ...PayInFields
+    }
+  }`
+
+export const BUY_CREDITS = gql`
+  ${PAY_IN_FIELDS}
+  mutation buyCredits($credits: Int!) {
+    buyCredits(credits: $credits) {
+      ...PayInFields
+    }
+  }`
+
+export const ACT_MUTATION = gql`
+  ${PAY_IN_FIELDS}
+  mutation act($id: ID!, $sats: Int!, $act: String, $hasSendWallet: Boolean) {
+    act(id: $id, sats: $sats, act: $act, hasSendWallet: $hasSendWallet) {
+      ...PayInFields
+    }
+  }`
+
+export const UPSERT_DISCUSSION = gql`
+  ${PAY_IN_FIELDS}
+  mutation upsertDiscussion($sub: String, $id: ID, $title: String!, $text: String,
+    $boost: Int, $forward: [ItemForwardInput], ${HASH_HMAC_INPUT_1}) {
+    upsertDiscussion(sub: $sub, id: $id, title: $title, text: $text, boost: $boost,
+      forward: $forward, ${HASH_HMAC_INPUT_2}) {
+      ...PayInFields
+    }
+  }`
+
+export const UPSERT_JOB = gql`
+  ${PAY_IN_FIELDS}
+  mutation upsertJob($sub: String!, $id: ID, $title: String!, $company: String!,
+    $location: String, $remote: Boolean, $text: String!, $url: String!, $boost: Int,
+    $status: String, $logo: Int) {
+    upsertJob(sub: $sub, id: $id, title: $title, company: $company,
+      location: $location, remote: $remote, text: $text,
+      url: $url, boost: $boost, status: $status, logo: $logo) {
+      ...PayInFields
+    }
+  }`
+
+export const UPSERT_LINK = gql`
+  ${PAY_IN_FIELDS}
+  mutation upsertLink($sub: String, $id: ID, $title: String!, $url: String!,
+    $text: String, $boost: Int, $forward: [ItemForwardInput], ${HASH_HMAC_INPUT_1}) {
+    upsertLink(sub: $sub, id: $id, title: $title, url: $url, text: $text,
+      boost: $boost, forward: $forward, ${HASH_HMAC_INPUT_2}) {
+      ...PayInFields
+    }
+  }`
+
+export const UPSERT_POLL = gql`
+  ${PAY_IN_FIELDS}
+  mutation upsertPoll($sub: String, $id: ID, $title: String!, $text: String,
+    $options: [String!]!, $boost: Int, $forward: [ItemForwardInput], $pollExpiresAt: Date,
+    $randPollOptions: Boolean, ${HASH_HMAC_INPUT_1}) {
+    upsertPoll(sub: $sub, id: $id, title: $title, text: $text,
+      options: $options, boost: $boost, forward: $forward, pollExpiresAt: $pollExpiresAt,
+      randPollOptions: $randPollOptions, ${HASH_HMAC_INPUT_2}) {
+      ...PayInFields
+    }
+  }`
+
+export const UPSERT_BOUNTY = gql`
+  ${PAY_IN_FIELDS}
+  mutation upsertBounty($sub: String, $id: ID, $title: String!, $bounty: Int!,
+    $text: String, $boost: Int, $forward: [ItemForwardInput]) {
+    upsertBounty(sub: $sub, id: $id, title: $title, bounty: $bounty, text: $text,
+      boost: $boost, forward: $forward) {
+      ...PayInFields
+    }
+  }`
+
+export const POLL_VOTE = gql`
+  ${PAY_IN_FIELDS}
+  mutation pollVote($id: ID!) {
+    pollVote(id: $id) {
+      ...PayInFields
+    }
+  }`
+
+export const UPSERT_BIO = gql`
+  ${PAY_IN_FIELDS}
+  mutation upsertBio($text: String!) {
+    upsertBio(text: $text) {
+      ...PayInFields
+    }
+  }`
+
+export const CREATE_COMMENT = gql`
+  ${PAY_IN_FIELDS}
+  mutation upsertComment($text: String!, $parentId: ID!) {
+    upsertComment(text: $text, parentId: $parentId) {
+      ...PayInFields
+    }
+  }`
+
+export const UPDATE_COMMENT = gql`
+  ${PAY_IN_FIELDS}
+  mutation upsertComment($id: ID!, $text: String!, $boost: Int, ${HASH_HMAC_INPUT_1}) {
+    upsertComment(id: $id, text: $text, boost: $boost, ${HASH_HMAC_INPUT_2}) {
+      ...PayInFields
+    }
+  }`
+
+export const UPSERT_SUB = gql`
+  ${PAY_IN_FIELDS}
+  mutation upsertSub($oldName: String, $name: String!, $desc: String, $baseCost: Int!,
+    $replyCost: Int!, $postTypes: [String!]!, $billingType: String!,
+    $billingAutoRenew: Boolean!, $moderated: Boolean!, $nsfw: Boolean!) {
+      upsertSub(oldName: $oldName, name: $name, desc: $desc, baseCost: $baseCost,
+        replyCost: $replyCost, postTypes: $postTypes, billingType: $billingType,
+        billingAutoRenew: $billingAutoRenew, moderated: $moderated, nsfw: $nsfw) {
+      ...PayInFields
+    }
+  }`
+
+export const UNARCHIVE_TERRITORY = gql`
+  ${PAY_IN_FIELDS}
+  mutation unarchiveTerritory($name: String!, $desc: String, $baseCost: Int!,
+    $replyCost: Int!, $postTypes: [String!]!, $billingType: String!,
+    $billingAutoRenew: Boolean!, $moderated: Boolean!, $nsfw: Boolean!) {
+      unarchiveTerritory(name: $name, desc: $desc, baseCost: $baseCost,
+        replyCost: $replyCost, postTypes: $postTypes, billingType: $billingType,
+        billingAutoRenew: $billingAutoRenew, moderated: $moderated, nsfw: $nsfw) {
+      ...PayInFields
+    }
+  }`
+
+export const SUB_PAY = gql`
+  ${PAY_IN_FIELDS}
+  mutation paySub($name: String!) {
+    paySub(name: $name) {
+      ...PayInFields
+    }
+  }`

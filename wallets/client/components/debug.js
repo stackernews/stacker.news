303f7fe54321ad038565e227f225c5ec61ca53b6
ekzyis <ek@stacker.news>
2025-07-21 20:08:44 +0200

    Show key hash information

--- wallets/client/components/debug.js
+++ wallets/client/components/debug.js
@@ -1,7 +1,11 @@
 import { formatBytes } from '@/lib/format'
 import { useEffect, useState } from 'react'
+import { useKeyHash } from '@/wallets/client/context'
+import { useRemoteKeyHash } from '@/wallets/client/hooks'
 
 export function WalletDebugSettings () {
+  const localKeyHash = useKeyHash()
+  const remoteKeyHash = useRemoteKeyHash()
   const [persistent, setPersistent] = useState(null)
   const [quota, setQuota] = useState(null)
   const [usage, setUsage] = useState(null)
@@ -43,7 +47,18 @@ export function WalletDebugSettings () {
         <div className='col text-nowrap'>storage remaining:</div>
         <div className='col'>{usage !== null && quota !== null ? formatBytes(quota - usage) : 'unknown'}</div>
       </div>
-      <div className='row' />
+      <div className='row'>
+        <div className='col text-nowrap'>device key hash:</div>
+        <div className='col'>{localKeyHash ? shortHash(localKeyHash) : 'unknown'}</div>
+      </div>
+      <div className='row'>
+        <div className='col text-nowrap'>server key hash:</div>
+        <div className='col'>{remoteKeyHash ? shortHash(remoteKeyHash) : 'unknown'}</div>
+      </div>
     </div>
   )
+}
+
+function shortHash (hash) {
+  return hash.slice(0, 6) + '...' + hash.slice(-6)
 }

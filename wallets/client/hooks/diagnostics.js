37251431fd491042cf5ffb8a1e6cc50e111edc56
ekzyis <ek@stacker.news>
2025-07-23 14:00:50 +0200

    Use me.privates.diagnostics as source of truth

--- wallets/client/hooks/diagnostics.js
+++ wallets/client/hooks/diagnostics.js
@@ -2,23 +2,22 @@ import { useMe } from '@/components/me'
 import { useToast } from '@/components/toast'
 import { SET_DIAGNOSTICS } from '@/fragments/users'
 import { useMutation } from '@apollo/client'
-import { useCallback, useState } from 'react'
+import { useCallback } from 'react'
 
 export function useDiagnostics () {
-  const { me } = useMe()
-  const [diagnostics, _setDiagnostics] = useState(me?.privates?.diagnostics ?? false)
+  const { me, refreshMe } = useMe()
   const [mutate] = useMutation(SET_DIAGNOSTICS)
   const toaster = useToast()
 
-  const setDiagnostics = useCallback(async (value) => {
+  const setDiagnostics = useCallback(async (diagnostics) => {
     try {
-      await mutate({ variables: { diagnostics: value } })
-      _setDiagnostics(value)
+      await mutate({ variables: { diagnostics } })
+      await refreshMe()
     } catch (err) {
       console.error('failed to toggle diagnostics:', err)
       toaster.danger('failed to toggle diagnostics')
     }
-  }, [mutate, toaster])
+  }, [mutate, toaster, refreshMe])
 
-  return [diagnostics, setDiagnostics]
+  return [me?.privates?.diagnostics ?? false, setDiagnostics]
 }

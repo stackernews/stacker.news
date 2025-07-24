e2a0f492f94b9d89db362c12e8e9fd3f55bc73a1
Soxasora <sora@soxa.dev>
2025-07-24 14:01:41 +0200

    fix: recurse through existing comments only if we're in the newComments subtree or if it's the start of a thread

--- components/show-new-comments.js
+++ components/show-new-comments.js
@@ -69,8 +69,8 @@ function prepareComments (data, client, newComments) {
 }
 
 // traverses all new comments and their children
-// if we're showing all new comments of a thread, we also consider their existing children
-function traverseNewComments (client, item, onLevel, threadComment = false, currentDepth = 1) {
+// if it's a thread, or we're in a new comment subtree, we also consider their existing children
+function traverseNewComments (client, item, onLevel, currentDepth = 1, inSubtree = false) {
   // if we're at the depth limit, stop traversing, we've reached the bottom of the visible thread
   if (currentDepth >= COMMENT_DEPTH_LIMIT) return
 
@@ -87,23 +87,23 @@ function traverseNewComments (client, item, onLevel, threadComment = false, curr
     // at each level, we can execute a callback passing the current item's new comments, depth and ID
     onLevel(freshNewComments, currentDepth, item.id)
 
+    // traverse the new comment's new comments
     for (const newComment of freshNewComments) {
-      traverseNewComments(client, newComment, onLevel, threadComment, currentDepth + 1)
+      traverseNewComments(client, newComment, onLevel, currentDepth + 1, true)
     }
   }
 
-  // if we're showing all new comments of a thread
-  // we consider every child comment recursively
-  if (threadComment && item.comments?.comments) {
+  // check for new comments in existing children, only if we're in a new comment subtree
+  if (inSubtree && item.comments?.comments) {
     for (const child of item.comments.comments) {
-      traverseNewComments(client, child, onLevel, threadComment, currentDepth + 1)
+      traverseNewComments(client, child, onLevel, currentDepth + 1, true)
     }
   }
 }
 
 // recursively processes and displays all new comments
 // handles comment injection at each level, respecting depth limits
-function injectNewComments (client, item, currentDepth, sort, threadComment = false) {
+function injectNewComments (client, item, currentDepth, sort, thread) {
   traverseNewComments(client, item, (newComments, depth, itemId) => {
     if (newComments.length > 0) {
       // traverseNewComments also passes the depth of the current item
@@ -114,11 +114,11 @@ function injectNewComments (client, item, currentDepth, sort, threadComment = fa
         commentUpdateFragment(client, itemId, (data) => prepareComments(data, client, newComments))
       }
     }
-  }, threadComment, currentDepth)
+  }, currentDepth, thread)
 }
 
 // counts all new comments of an item
-function countAllNewComments (client, item, thread = false, currentDepth = 1) {
+function countAllNewComments (client, item, currentDepth = 1, thread) {
   let newCommentsCount = 0
   let threadChildren = false
 
@@ -130,7 +130,7 @@ function countAllNewComments (client, item, thread = false, currentDepth = 1) {
     if (depth > 1 && newComments.length > 0) {
       threadChildren = true
     }
-  }, thread, currentDepth)
+  }, currentDepth, thread)
 
   return { newCommentsCount, threadChildren }
 }
@@ -165,7 +165,7 @@ export function ShowNewComments ({ topLevel, item, sort, depth = 0 }) {
 
   // recurse through all new comments and their children
   // if the item is a thread, we consider every existing child comment
-  const { newCommentsCount, threadChildren } = countAllNewComments(client, item, thread, depth)
+  const { newCommentsCount, threadChildren } = countAllNewComments(client, item, depth, thread)
 
   // only if the item is a thread and its children have new comments, we show "show all new comments"
   const threadComment = thread && threadChildren

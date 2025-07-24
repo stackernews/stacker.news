9613cc8f413aab99e08df058165764bd1626c5a6
Soxasora <sora@soxa.dev>
2025-07-24 15:00:28 +0200

    cleanup: better comment

--- components/show-new-comments.js
+++ components/show-new-comments.js
@@ -93,7 +93,8 @@ function traverseNewComments (client, item, onLevel, currentDepth = 1, inSubtree
     }
   }
 
-  // check for new comments in existing children, only if we're in a new comment subtree
+  // check for new comments in existing children
+  // only if we're in a new comment subtree, or it's a thread
   if (inSubtree && item.comments?.comments) {
     for (const child of item.comments.comments) {
       traverseNewComments(client, child, onLevel, currentDepth + 1, true)
@@ -164,7 +165,7 @@ export function ShowNewComments ({ topLevel, item, sort, depth = 0 }) {
   const thread = item.path?.split('.').length === 2
 
   // recurse through all new comments and their children
-  // if the item is a thread, we consider every existing child comment
+  // if the item is a thread, we also consider all of their existing children
   const { newCommentsCount, threadChildren } = countAllNewComments(client, item, depth, thread)
 
   // only if the item is a thread and its children have new comments, we show "show all new comments"

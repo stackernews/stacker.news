b4240ba5ffaa8593165d6cfdea76de4983f2cf0c
ekzyis <ek@stacker.news>
2025-07-30 17:20:06 +0200

    Fix missing validation for NWC receive

--- wallets/server/protocols/nwc.js
+++ wallets/server/protocols/nwc.js
@@ -1,4 +1,4 @@
-import { nwcTryRun } from '@/wallets/lib/protocols/nwc'
+import { nwcTryRun, supportedMethods } from '@/wallets/lib/protocols/nwc'
 
 export const name = 'NWC'
 
@@ -12,6 +12,20 @@ export async function createInvoice ({ msats, description, expiry }, { url }, { 
 }
 
 export async function testCreateInvoice ({ url }, { signal }) {
+  const supported = await supportedMethods(url, { signal })
+  const supports = (method) => supported.includes(method)
+
+  if (!supports('make_invoice')) {
+    throw new Error('make_invoice not supported')
+  }
+
+  const mustNotSupport = ['pay_invoice', 'multi_pay_invoice', 'pay_keysend', 'multi_pay_keysend']
+  for (const method of mustNotSupport) {
+    if (supports(method)) {
+      throw new Error(`${method} must not be supported`)
+    }
+  }
+
   return await createInvoice(
     { msats: 1000, description: 'SN test invoice', expiry: 1 },
     { url },

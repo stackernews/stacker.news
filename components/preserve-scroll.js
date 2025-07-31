f61ee1f5af1da61833471de2a06ed4b1176acd68
Soxasora <sora@soxa.dev>
2025-07-31 12:18:41 +0200

    reliably preserve scroll position by tracking a reference found at the center of the viewport; cleanup: add more comments, add cleanup function

--- components/preserve-scroll.js
+++ components/preserve-scroll.js
@@ -2,35 +2,51 @@ export default function preserveScroll (callback) {
   // preserve the actual scroll position
   const scrollTop = window.scrollY
 
-  // if the scroll position is at the top, we don't need to preserve it
+  // if the scroll position is at the top, we don't need to preserve it, just call the callback
   if (scrollTop <= 0) {
     callback()
     return
   }
 
-  // get the initial document height
-  const initialDocumentHeight = document.documentElement.scrollHeight
+  // get a reference element at the center of the viewport to track if content is added above it
+  const ref = document.elementFromPoint(window.innerWidth / 2, window.innerHeight / 2)
+  const refTop = ref ? ref.getBoundingClientRect().top + scrollTop : scrollTop
 
   // observe the document for changes in height
   const observer = new window.MutationObserver(() => {
     // request animation frame to ensure the DOM is updated
     window.requestAnimationFrame(() => {
-      const heightDifference = document.documentElement.scrollHeight - initialDocumentHeight
+      // we can't proceed if we couldn't find a traceable reference element
+      if (!ref) {
+        cleanup()
+        return
+      }
 
-      // if we have a height difference, we need to scroll to the new position
-      if (heightDifference > 0) {
+      // get the new position of the reference element along with the new scroll position
+      const newRefTop = ref ? ref.getBoundingClientRect().top + window.scrollY : window.scrollY
+      // has the reference element moved?
+      const refMoved = newRefTop - refTop
+
+      // if the reference element moved, we need to scroll to the new position
+      if (refMoved > 0) {
         window.scrollTo({
-          top: scrollTop + heightDifference,
+          top: scrollTop + refMoved,
           behavior: 'instant'
         })
       }
 
-      observer.disconnect()
+      cleanup()
     })
   })
 
+  const timeout = setTimeout(() => cleanup(), 1000) // fallback
+
+  function cleanup () {
+    clearTimeout(timeout)
+    observer.disconnect()
+  }
+
   observer.observe(document.body, { childList: true, subtree: true })
-  setTimeout(() => observer.disconnect(), 1000) // fallback??
 
   callback()
 }

7f3013dd5f6e94c7e0d339695e5e83d6b35c7299
Soxasora <sora@soxa.dev>
2025-08-02 02:29:47 +0200

    store comment refs array in a ref to avoid state re-renders

--- components/use-live-comments-navigator.js
+++ components/use-live-comments-navigator.js
@@ -1,53 +1,75 @@
-import { useCallback, useState, useEffect } from 'react'
+import { useCallback, useState, useEffect, useRef } from 'react'
 import ArrowUp from '../svgs/arrow-up-s-line.svg'
 import ArrowDown from '../svgs/arrow-down-s-line.svg'
 import styles from './comment.module.css'
+import { useFavicon } from './favicon'
 
 // TODO: mega cleanup after solid decision on design pattern
 export function useLiveCommentsNavigator () {
-  const [commentRefs, setCommentRefs] = useState([])
+  const { hasNewComments, setHasNewComments } = useFavicon()
+  const [commentCount, setCommentCount] = useState(0)
   const [currentIndex, setCurrentIndex] = useState(-1)
+
+  const commentRefs = useRef([])
 
   // add a new comment ref to the list
   const trackNewComment = useCallback((commentRef) => {
     if (!commentRef?.current) return
 
     // dedupe and store the ref
-    setCommentRefs(prevRefs => {
-      if (prevRefs.some(ref => ref.current === commentRef.current)) return prevRefs
-      const newRefs = [...prevRefs, commentRef]
-      return newRefs
-    })
+    if (!commentRefs.current.some(ref => ref.current === commentRef.current)) {
+      commentRefs.current.push(commentRef)
+    }
+
+    setCommentCount(commentRefs.current.length)
   }, [])
 
   // clear the list of refs and reset the current index to -1
   const clearCommentRefs = useCallback(() => {
+    // reset navigator
+    commentRefs.current = []
     setCurrentIndex(-1)
-    setCommentRefs([])
-  }, [])
+    setCommentCount(0)
+
+    // reset favicon
+    if (hasNewComments) {
+      setHasNewComments(false)
+    }
+  }, [hasNewComments, setHasNewComments])
 
   const scrollToComment = useCallback((direction) => {
-    let index = currentIndex
+    const refs = commentRefs.current
+
+    let newIndex
     if (direction === 'prev') {
-      index = Math.max(index - 1, 0)
+      newIndex = Math.max(currentIndex - 1, 0)
     } else if (direction === 'next') {
-      index = Math.min(index + 1, commentRefs.length - 1)
+      newIndex = Math.min(currentIndex + 1, commentCount - 1)
     }
 
-    setCurrentIndex(index)
+    setCurrentIndex(newIndex)
 
-    const ref = commentRefs[index]
-    if (!ref) return
-    ref.current.scrollIntoView({ behavior: 'smooth', block: 'start' })
+    const ref = refs[newIndex]
+    if (!ref?.current) return
 
     // clear any conflicting classes
-    ref.current.classList.remove('outline-it', 'outline-new-comment', 'outline-new-injected-comment', 'outline-new-comment-unset')
+    ref.current.classList.remove(
+      'outline-it',
+      'outline-new-comment',
+      'outline-new-injected-comment',
+      'outline-new-comment-unset'
+    )
 
-    // when the animation ends, remove the temporary outline and add the unset class: we viewed the comment
-    ref.current.addEventListener('animationend', () => {
+    // smoothly scroll to the start of the comment
+    ref.current.scrollIntoView({ behavior: 'smooth', block: 'start' })
+
+    const clearOutline = () => {
       ref.current.classList.remove('outline-it')
       ref.current.classList.add('outline-new-comment-unset')
-    }, { once: true })
+    }
+
+    // when the animation ends, remove the temporary outline and add the unset class: we viewed the comment
+    ref.current.addEventListener('animationend', clearOutline, { once: true })
 
     // requestAnimationFrame to ensure the removal is processed before adding
     window.requestAnimationFrame(() => {
@@ -55,10 +77,10 @@ export function useLiveCommentsNavigator () {
     })
 
     // we reached the end of the new comments, clear the tracked refs
-    if (index === commentRefs.length - 1) {
+    if (newIndex === commentCount - 1) {
       clearCommentRefs()
     }
-  }, [commentRefs, clearCommentRefs, currentIndex])
+  }, [clearCommentRefs, currentIndex, commentCount])
 
   // clear the refs when the component unmounts
   useEffect(() => {
@@ -72,13 +94,15 @@ export function useLiveCommentsNavigator () {
     scrollToComment,
     clearCommentRefs,
     currentIndex,
-    commentCount: commentRefs.length
+    commentCount
   }
 }
 
 export function LiveCommentsNavigator ({ navigator }) {
   const { commentCount, currentIndex, scrollToComment, clearCommentRefs } = navigator
   if (!commentCount) return null
+
+  console.log('commentCount', commentCount)
 
   return (
     <span className={`${styles.commentNavigator} fw-bold`}>

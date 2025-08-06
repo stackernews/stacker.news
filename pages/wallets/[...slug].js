d73ddf834d973dd1659afcd8766c196d4eca1f77
ekzyis <ek@stacker.news>
2025-08-06 02:14:55 +0200

    Fix back/forward navigation with useData hook

--- pages/wallets/[...slug].js
+++ pages/wallets/[...slug].js
@@ -1,8 +1,11 @@
 import { getGetServerSideProps } from '@/api/ssrApollo'
+import { useData } from '@/components/use-data'
 import { WalletForms as WalletFormsComponent } from '@/wallets/client/components'
 import { WALLET } from '@/wallets/client/fragments'
 import { useDecryptedWallet } from '@/wallets/client/hooks'
 import { unurlify } from '@/wallets/lib/util'
+import { useQuery } from '@apollo/client'
+import { useRouter } from 'next/router'
 
 const variablesFunc = params => {
   const id = Number(params.slug[0])
@@ -11,6 +14,11 @@ const variablesFunc = params => {
 export const getServerSideProps = getGetServerSideProps({ query: WALLET, variables: variablesFunc, authRequired: true })
 
 export default function WalletForms ({ ssrData }) {
-  const decryptedWallet = useDecryptedWallet(ssrData?.wallet)
-  return <WalletFormsComponent wallet={decryptedWallet ?? ssrData?.wallet} />
+  const router = useRouter()
+  const variables = variablesFunc(router.query)
+  const { data } = useQuery(WALLET, { variables })
+  const dat = useData(data, ssrData)
+
+  const decryptedWallet = useDecryptedWallet(dat?.wallet)
+  return <WalletFormsComponent wallet={decryptedWallet ?? dat?.wallet} />
 }

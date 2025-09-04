197cb1cc5454477774b3982b92e35c8cae74f8ec
Soxasora <sora@soxa.dev>
2025-09-04 16:14:20 +0200

    generic useCommentsView hook

--- components/use-comments-view.js
+++ components/use-comments-view.js
@@ -1,16 +1,15 @@
-// components/use-comments-view.js
 import { useMutation } from '@apollo/client'
-import { useMe } from './me'
-import { useRoot } from './root'
+import { useCallback } from 'react'
 import { UPDATE_ITEM_USER_VIEW } from '@/fragments/items'
 import { commentsViewedAfterComment } from '@/lib/new-comments'
-import { useCallback } from 'react'
+import { useMe } from './me'
+import { useRoot } from './root'
 
-export default function useCommentsView () {
+export default function useCommentsView ({ updateCache = true } = {}) {
   const { me } = useMe()
   const root = useRoot()
 
-  const [updateCommentsViewAt] = useMutation(UPDATE_ITEM_USER_VIEW, {
+  const [updateCommentsViewAt] = useMutation(UPDATE_ITEM_USER_VIEW, updateCache && {
     update (cache, { data: { updateCommentsViewAt } }) {
       cache.modify({
         id: `Item:${root.id}`,
@@ -19,15 +18,15 @@ export default function useCommentsView () {
     }
   })
 
-  // TODO: comment?
-  const markViewedAt = useCallback((createdAt, { rootId, ncomments } = {}) => {
+  // update the meCommentsViewedAt field for the root item
+  const markViewedAt = useCallback((latest, { rootId, ncomments } = {}) => {
     const id = rootId || root.id
     if (me?.id) {
-      updateCommentsViewAt({ variables: { id, meCommentsViewedAt: createdAt } })
+      updateCommentsViewAt({ variables: { id, meCommentsViewedAt: latest } })
     } else {
-      commentsViewedAfterComment(id, createdAt, ncomments)
+      commentsViewedAfterComment(id, latest, ncomments)
     }
-  }, [me?.id, root?.id, updateCommentsViewAt])
+  }, [me?.id, root?.id, updateCommentsViewAt, updateCache])
 
   return markViewedAt
 }

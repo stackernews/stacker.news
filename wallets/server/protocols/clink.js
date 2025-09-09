0d53f40f612ac75aa3285050c311b45ab97b7b83
ekzyis <ek@stacker.news>
2025-09-09 19:56:07 +0200

    First attempt of attaching CLINK receive

--- wallets/server/protocols/clink.js
+++ wallets/server/protocols/clink.js
@@ -1,12 +1,36 @@
+import { msatsToSats } from '@/lib/format'
+import { ClinkSDK, decodeBech32, generateSecretKey } from '@shocknet/clink-sdk'
+
 export const name = 'CLINK'
 
-export function createInvoice (
+export async function createInvoice (
   { msats, description, expiry },
   { noffer },
   { signal }) {
-  // TODO(clink): implement
+  const { data: { offer, relay, pubkey } } = decodeBech32(noffer)
+
+  const sdk = new ClinkSDK({
+    privateKey: generateSecretKey(),
+    relays: [relay],
+    toPubKey: pubkey
+  })
+
+  const request = { offer, amount_sats: msatsToSats(msats) }
+  return await new Promise((resolve, reject) => {
+    // TODO(clink): for some reason this throws 'Invalid Amount' but amount_sats is a number
+    sdk.Noffer(request)
+      .then(response => {
+        if ('bolt11' in response) {
+          return resolve(response.bolt11)
+        }
+        reject(new Error(response.error))
+      })
+  })
 }
 
-export function testCreateInvoice (config, opts) {
-  // TODO(clink): implement
+export async function testCreateInvoice ({ noffer }, { signal }) {
+  return await createInvoice(
+    { msats: 1000, description: 'SN test invoice', expiry: 1 },
+    { noffer },
+    { signal })
 }

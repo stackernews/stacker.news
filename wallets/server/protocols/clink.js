6957ce0f6755c301c45c081f82ce3bfd826f4f95
ekzyis <ek@stacker.news>
2025-09-09 22:24:09 +0200

    Working example of attaching CLINK receive

--- wallets/server/protocols/clink.js
+++ wallets/server/protocols/clink.js
@@ -1,5 +1,5 @@
 import { msatsToSats } from '@/lib/format'
-import { ClinkSDK, decodeBech32, generateSecretKey } from '@shocknet/clink-sdk'
+import { decodeBech32, generateSecretKey, SendNofferRequest, SimplePool } from '@shocknet/clink-sdk'
 
 export const name = 'CLINK'
 
@@ -9,28 +9,28 @@ export async function createInvoice (
   { signal }) {
   const { data: { offer, relay, pubkey } } = decodeBech32(noffer)
 
-  const sdk = new ClinkSDK({
-    privateKey: generateSecretKey(),
-    relays: [relay],
-    toPubKey: pubkey
-  })
-
+  // https://github.com/shocknet/clink-demo/blob/94ccde0c984c65b294b73cdd300b3a6868a7fd0c/src/index.ts#L106
+  const pool = new SimplePool()
+  const sk = generateSecretKey()
   const request = { offer, amount_sats: msatsToSats(msats) }
-  return await new Promise((resolve, reject) => {
-    // TODO(clink): for some reason this throws 'Invalid Amount' but amount_sats is a number
-    sdk.Noffer(request)
-      .then(response => {
-        if ('bolt11' in response) {
-          return resolve(response.bolt11)
-        }
-        reject(new Error(response.error))
-      })
-  })
+
+  let response
+  try {
+    response = await SendNofferRequest(pool, sk, [relay], pubkey, request)
+  } catch (e) {
+    throw typeof e === 'string' ? new Error(e) : e
+  }
+
+  if ('bolt11' in response && typeof response.bolt11 === 'string') {
+    return response.bolt11
+  }
+  throw new Error(response.error)
 }
 
 export async function testCreateInvoice ({ noffer }, { signal }) {
   return await createInvoice(
-    { msats: 1000, description: 'SN test invoice', expiry: 1 },
+    // lnpub minimum range seems to be 10 sats by default so we use 100 sats
+    { msats: 100e3, description: 'SN test invoice', expiry: 1 },
     { noffer },
     { signal })
 }

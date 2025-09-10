efc07da4eadd172892b30254ddd6cf7588b57a12
ekzyis <ek@stacker.news>
2025-09-10 17:01:34 +0200

    Fix row in Wallet and WalletProtocol not deleted

--- prisma/migrations/20250910153121_delete_nwc_recv_with_payments/migration.sql
+++ prisma/migrations/20250910153121_delete_nwc_recv_with_payments/migration.sql
@@ -1,9 +1,18 @@
-DELETE FROM "WalletRecvNWC"
-WHERE "id" IN (
-    7,
-    67,
-    140,
-    157,
-    158,
-    166
-);
+WITH deleted_protocols AS (
+    DELETE FROM "WalletProtocol"
+    WHERE id IN (
+        SELECT "protocolId" FROM "WalletRecvNWC"
+        WHERE id IN (
+            7,
+            67,
+            140,
+            157,
+            158,
+            166
+        )
+    )
+    RETURNING "walletId"
+)
+DELETE FROM "Wallet"
+WHERE id IN (SELECT "walletId" FROM deleted_protocols)
+AND NOT EXISTS (SELECT 1 FROM "WalletProtocol" WHERE "walletId" = "Wallet"."id");

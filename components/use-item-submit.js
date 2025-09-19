2cbadb0e39e2700ef0f4cfd20a4c899f77634132
ekzyis <ek@stacker.news>
2025-09-19 15:55:57 +0200

    Fix draft gone if wallet prompt closed

--- components/use-item-submit.js
+++ components/use-item-submit.js
@@ -8,7 +8,7 @@ import { RETRY_PAID_ACTION } from '@/fragments/paidAction'
 import gql from 'graphql-tag'
 import { USER_ID } from '@/lib/constants'
 import { useMe } from './me'
-import { useWalletRecvPrompt, WalletPromptClosed } from '@/wallets/client/hooks'
+import { useWalletRecvPrompt } from '@/wallets/client/hooks'
 
 // this is intented to be compatible with upsert item mutations
 // so that it can be reused for all post types and comments and we don't have
@@ -27,12 +27,7 @@ export default function useItemSubmit (mutation,
 
   return useCallback(
     async ({ boost, crosspost, title, options, bounty, status, ...values }, { resetForm }) => {
-      try {
-        await walletPrompt()
-      } catch (err) {
-        if (err instanceof WalletPromptClosed) return
-        throw err
-      }
+      await walletPrompt()
 
       if (options) {
         // remove existing poll options since else they will be appended as duplicates

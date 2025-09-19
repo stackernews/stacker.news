7572d38df546b98d5e3a2f286f327d46d6b1c8d7
ekzyis <ek@stacker.news>
2025-09-19 13:22:12 +0200

    Remove migration code for old client-side wallets

--- wallets/client/context/provider.js
+++ wallets/client/context/provider.js
@@ -1,6 +1,6 @@
 import { createContext, useContext, useReducer } from 'react'
 import walletsReducer from './reducer'
-import { useServerWallets, useAutomatedRetries, useKeyInit, useWalletMigration } from './hooks'
+import { useServerWallets, useAutomatedRetries, useKeyInit, useDeleteLocalWallets } from './hooks'
 import { WebLnProvider } from '@/wallets/lib/protocols/webln'
 
 // https://react.dev/learn/scaling-up-with-reducer-and-context
@@ -80,14 +80,7 @@ function WalletHooks ({ children }) {
   useServerWallets()
   useAutomatedRetries()
   useKeyInit()
-
-  // TODO(wallet-v2): remove migration code
-  // =============================================================
-  // ****** Below is the migration code for WALLET v1 -> v2 ******
-  //   remove when we can assume migration is complete (if ever)
-  // =============================================================
-
-  useWalletMigration()
+  useDeleteLocalWallets()
 
   return children
 }

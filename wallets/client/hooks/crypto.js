604df18e59378ee07afbe0ca4591cd06bbe66044
ekzyis <ek@stacker.news>
2025-09-20 11:48:59 +0200

    Create wallets/lib/crypto.js

--- wallets/client/hooks/crypto.js
+++ wallets/client/hooks/crypto.js
@@ -1,10 +1,10 @@
 import { useCallback, useMemo } from 'react'
-import { fromHex, toHex } from '@/lib/hex'
 import { useMe } from '@/components/me'
 import { useIndexedDB } from '@/components/use-indexeddb'
 import { SET_KEY, useKey, useKeyHash, useWalletsDispatch } from '@/wallets/client/context'
 import { useUpdateKeyHash, useWalletLogger } from '@/wallets/client/hooks'
 import { generateRandomPassphrase } from '@/wallets/client/hooks/passphrase'
+import { deriveKey, decrypt as _decrypt, encrypt as _encrypt } from '@/wallets/lib/crypto'
 
 export class CryptoKeyRequiredError extends Error {
   constructor () {
@@ -92,82 +92,6 @@ export function useKeySalt () {
   // TODO(wallet-v2): random salt
   const { me } = useMe()
   return `stacker${me?.id}`
-}
-
-export async function deriveKey (passphrase, salt) {
-  const enc = new TextEncoder()
-
-  const keyMaterial = await window.crypto.subtle.importKey(
-    'raw',
-    enc.encode(passphrase),
-    { name: 'PBKDF2' },
-    false,
-    ['deriveKey']
-  )
-
-  const key = await window.crypto.subtle.deriveKey(
-    {
-      name: 'PBKDF2',
-      salt: enc.encode(salt),
-      // 600,000 iterations is recommended by OWASP
-      // see https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html#pbkdf2
-      iterations: 600_000,
-      hash: 'SHA-256'
-    },
-    keyMaterial,
-    { name: 'AES-GCM', length: 256 },
-    true,
-    ['encrypt', 'decrypt']
-  )
-
-  const rawKey = await window.crypto.subtle.exportKey('raw', key)
-  const hash = toHex(await window.crypto.subtle.digest('SHA-256', rawKey))
-  const unextractableKey = await window.crypto.subtle.importKey(
-    'raw',
-    rawKey,
-    { name: 'AES-GCM' },
-    false,
-    ['encrypt', 'decrypt']
-  )
-
-  return {
-    key: unextractableKey,
-    hash
-  }
-}
-
-async function _encrypt ({ key, hash }, value) {
-  // random IVs are _really_ important in GCM: reusing the IV once can lead to catastrophic failure
-  // see https://crypto.stackexchange.com/questions/26790/how-bad-it-is-using-the-same-iv-twice-with-aes-gcm
-  // 12 bytes (96 bits) is the recommended IV size for AES-GCM
-  const iv = window.crypto.getRandomValues(new Uint8Array(12))
-  const encoded = new TextEncoder().encode(JSON.stringify(value))
-  const encrypted = await window.crypto.subtle.encrypt(
-    {
-      name: 'AES-GCM',
-      iv
-    },
-    key,
-    encoded
-  )
-  return {
-    keyHash: hash,
-    iv: toHex(iv.buffer),
-    value: toHex(encrypted)
-  }
-}
-
-async function _decrypt (key, { iv, value }) {
-  const decrypted = await window.crypto.subtle.decrypt(
-    {
-      name: 'AES-GCM',
-      iv: fromHex(iv)
-    },
-    key,
-    fromHex(value)
-  )
-  const decoded = new TextDecoder().decode(decrypted)
-  return JSON.parse(decoded)
 }
 
 export function useGenerateRandomKey () {

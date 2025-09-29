758109a79570653c58f63331ce289f0db526b2b4
Soxasora <sora@soxa.dev>
2025-09-29 16:59:48 +0200

    tweaks to formik bridge onChange save actions

--- components/lexical/plugins/onchange/formikbridge.js
+++ components/lexical/plugins/onchange/formikbridge.js
@@ -2,11 +2,11 @@ import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext
 import { useFormikContext } from 'formik'
 import { useEffect, useContext, useCallback } from 'react'
 import { $convertToMarkdownString, $convertFromMarkdownString } from '@lexical/markdown'
-import { $isCodeNode } from '@lexical/code'
 import { $getRoot, createEditor } from 'lexical'
 import DefaultNodes from '@/lib/lexical/nodes'
 import SN_TRANSFORMERS from '@/lib/lexical/transformers'
 import { StorageKeyPrefixContext } from '@/components/form'
+import { $isMarkdownNode } from '@/lib/lexical/nodes/markdownnode'
 
 function parseMarkdown (editor, content) {
   const markdown = content.getTextContent()
@@ -41,6 +41,29 @@ export default function FormikBridgePlugin ({ name }) {
     }
   }, [storageKey])
 
+  const lexicalFormikify = useCallback((editorState) => {
+    console.log('hyperTransform')
+    editorState.read(() => {
+      const root = $getRoot()
+      const firstChild = root.getFirstChild()
+
+      let lexicalState = editorState.toJSON()
+      let markdown = ''
+
+      if ($isMarkdownNode(firstChild)) {
+        ({ markdown, lexicalState } = parseMarkdown(editor, firstChild))
+      } else {
+        markdown = $convertToMarkdownString(SN_TRANSFORMERS, undefined, true)
+      }
+
+      // useless to update storage if the values are the same
+      if (values.text === markdown && values.lexicalState === JSON.stringify(lexicalState)) return
+      onChangeInner(JSON.stringify(lexicalState))
+      setFieldValue('text', markdown)
+      setFieldValue('lexicalState', JSON.stringify(lexicalState))
+    })
+  }, [editor, setFieldValue, values, onChangeInner])
+
   useEffect(() => {
     if (storageKey) {
       const value = window.localStorage.getItem(storageKey)
@@ -57,32 +80,10 @@ export default function FormikBridgePlugin ({ name }) {
   }, [storageKey, setFieldValue, name])
 
   useEffect(() => {
-    // probably we need to debounce this
-    // holy shit this is a mess
     return editor.registerUpdateListener(({ editorState }) => {
-      editorState.read(() => {
-        const root = $getRoot()
-        const firstChild = root.getFirstChild()
-
-        let lexicalState = editorState.toJSON()
-        let markdown = ''
-
-        const isMarkdownMode = $isCodeNode(firstChild) && firstChild.getLanguage() === 'markdown'
-
-        // Markdown Mode (codeblock)
-        if (isMarkdownMode) {
-          ({ markdown, lexicalState } = parseMarkdown(editor, firstChild))
-        } else {
-          markdown = $convertToMarkdownString(SN_TRANSFORMERS, undefined, true)
-        }
-        console.log('lexicalState', lexicalState)
-        if (values.text === markdown) return
-        onChangeInner(JSON.stringify(lexicalState))
-        setFieldValue('text', markdown)
-        setFieldValue('lexicalState', JSON.stringify(lexicalState))
-      })
+      lexicalFormikify(editorState)
     })
-  }, [editor, setFieldValue, values])
+  }, [editor, lexicalFormikify])
 
   return null
 }

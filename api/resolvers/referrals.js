ca04d8d2fc2814bc847153c92609b2555ebaf0ff
k00b <k00b@stacker.news>
2025-10-22 16:43:33 -0500

    make defunct referrals query not throw

--- api/resolvers/referrals.js
+++ api/resolvers/referrals.js
@@ -1,9 +1,10 @@
 export default {
   Query: {
-    // TODO: need a new aggreation for referrals probably
-    // or just remove the notion of one day referrals
+    // TODO: i plan to remove the notion of one day referrals
+    // from rewards ... if a stacker does not have a signup referrer,
+    // the one day refer will be who gets 10% of rewards
     referrals: async (parent, { when, from, to }, { models, me }) => {
-      return null
+      return []
     }
   }
 }

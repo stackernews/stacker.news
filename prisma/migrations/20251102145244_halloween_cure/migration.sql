42e2f9647cbab6af549f7519237f7b2a44608267
ed-kung <edward.kung@gmail.com>
2025-11-02 19:14:33 -0800

    fix prisma migration

--- prisma/migrations/20251102145244_halloween_cure/migration.sql
+++ prisma/migrations/20251102145244_halloween_cure/migration.sql
@@ -1,5 +1,5 @@
 -- AlterTable
-ALTER TABLE "users" ADD COLUMN "cured" BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE "users" ADD COLUMN     "cured" BOOLEAN NOT NULL DEFAULT false;
 
 -- CreateTable
 CREATE TABLE "Cure" (
@@ -10,7 +10,7 @@ CREATE TABLE "Cure" (
     "curerId" INTEGER,
 
     CONSTRAINT "Cure_pkey" PRIMARY KEY ("id")
-)
+);
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Cure_cureeId_unique" ON "Cure"("cureeId");
@@ -19,13 +19,13 @@ CREATE UNIQUE INDEX "Cure_cureeId_unique" ON "Cure"("cureeId");
 CREATE UNIQUE INDEX "Cure_itemActId_unique" ON "Cure"("itemActId");
 
 -- CreateIndex
-CREATE INDEX "Cure_curerId_index" ON "Cure"("curerId");
+CREATE INDEX "Cure_curerId_idx" ON "Cure"("curerId");
 
 -- AddForeignKey
 ALTER TABLE "Cure" ADD CONSTRAINT "Cure_cureeId_fkey" FOREIGN KEY ("cureeId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Cure" ADD CONSTRAINT "Cure_curerId_fkey" FOREIGN KEY ("curerId") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "Cure" ADD CONSTRAINT "Cure_curerId_fkey" FOREIGN KEY ("curerId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Cure" ADD CONSTRAINT "Cure_itemActId_fkey" FOREIGN KEY ("itemActId") REFERENCES "ItemAct"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "Cure" ADD CONSTRAINT "Cure_itemActId_fkey" FOREIGN KEY ("itemActId") REFERENCES "ItemAct"("id") ON DELETE CASCADE ON UPDATE CASCADE;

26bcab2dfd741677d213191f9513b7efd7cf4a84
k00b <k00b@stacker.news>
2025-11-05 19:16:21 -0600

    mute invoice expire timer

--- components/payIn/status.js
+++ components/payIn/status.js
@@ -17,7 +17,7 @@ export function PayInStatus ({ payIn }) {
       {(payIn.payInState === 'PAID' && <><Check width={statusIconSize} height={statusIconSize} className='fill-success' /><StatusText color='success'>{payIn.mcost > 0 ? 'paid' : 'free'}</StatusText></>) ||
         ((payIn.payInState === 'FAILED' || payIn.payInState === 'CANCELLED' || payIn.payInState === 'FORWARD_FAILED') && <><ThumbDown width={statusIconSize} height={statusIconSize} className='fill-danger' /><StatusText color='danger'>failed</StatusText></>) ||
         ((payIn.payInState === 'FORWARDING' || payIn.payInState === 'FORWARDED' || !payIn.payerPrivates.payInBolt11) && <><Moon width={statusIconSize} height={statusIconSize} className='spin fill-grey' /><StatusText color='muted'>settling</StatusText></>) ||
-        (<CompactLongCountdown date={payIn.payerPrivates.payInBolt11.expiresAt} />)}
+        (<CompactLongCountdown className='text-muted' date={payIn.payerPrivates.payInBolt11.expiresAt} />)}
     </div>
   )
 }

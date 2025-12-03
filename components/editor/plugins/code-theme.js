4beea13822527b6d773384d9f7c86f6a1bfe1eb5
Soxasora <sora@soxa.dev>
2025-12-03 19:24:17 +0100

    change codeblock theme via command instead of mutating the editor

--- components/editor/plugins/code-theme.js
+++ components/editor/plugins/code-theme.js
@@ -1,6 +1,7 @@
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext'
 import { useEffect } from 'react'
 import useDarkMode from '@/components/dark-mode'
+import { UPDATE_CODE_THEME_COMMAND } from '@/lib/lexical/exts/shiki'
 
 /** syncs code block syntax highlighting theme with site dark mode */
 export function CodeThemePlugin () {
@@ -10,9 +11,8 @@ export function CodeThemePlugin () {
   const theme = darkMode ? 'github-dark-default' : 'github-light-default'
 
   useEffect(() => {
-    if (!editor._updateCodeTheme) return
-    return editor._updateCodeTheme(theme)
-  }, [darkMode, theme])
+    return editor.dispatchCommand(UPDATE_CODE_THEME_COMMAND, theme)
+  }, [editor, darkMode, theme])
 
   return null
 }

0956b9c4d11ad1afb0c78b1900012e71a569a939
Soxasora <sora@soxa.dev>
2025-12-16 13:13:38 +0100

    clarify use-callback-ref utility

--- components/use-callback-ref.js
+++ components/use-callback-ref.js
@@ -2,6 +2,10 @@ import { useState, useCallback } from 'react'
 
 /**
  * a hook that returns a ref and a callback to set the ref.
+ *
+ * especially useful for refs that may not be available immediately
+ *
+ * an example can be the Lexical Reader that is dynamically loaded, its ref is not available immediately.
  */
 export default function useCallbackRef (initialRef = null) {
   const [ref, setRef] = useState(initialRef)

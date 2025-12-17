50b499ac9e8c1500abf0a2700f1bccbd2b239965
k00b <k00b@stacker.news>
2025-12-17 14:50:42 -0600

    squash migrations

--- prisma/migrations/20251217001811_sub_rank_indices/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateIndex
-CREATE INDEX "Item_subName_created_at_idx" ON "Item"("subName", "created_at");
-
--- CreateIndex
-CREATE INDEX "Item_subName_ranktop_idx" ON "Item"("subName", "ranktop");
-
--- CreateIndex
-CREATE INDEX "Item_subName_rankhot_idx" ON "Item"("subName", "rankhot");

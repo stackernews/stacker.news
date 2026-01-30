42f23ef76b7ead829aad3b15956aa289aa87e06b
k00b <k00b@stacker.news>
2026-01-29 18:10:43 -0600

    remove reference to dead error

--- components/form.js
+++ components/form.js
@@ -31,7 +31,6 @@ import { useShowModal } from './modal'
 import dynamic from 'next/dynamic'
 import { useIsClient } from './use-client'
 import PageLoading from './page-loading'
-import { WalletPromptClosed } from '@/wallets/client/hooks'
 import { SNEditor } from './editor'
 export { MultiSelect } from './multi-select'
 export class SessionRequiredError extends Error {
@@ -789,7 +788,6 @@ export function Form ({
         await onSubmit(values, ...args)
       }
     } catch (err) {
-      if (err instanceof WalletPromptClosed) return
       console.log(err.message, err)
       toaster.danger(err.message ?? err.toString?.())
       return

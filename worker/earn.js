f5854b495867440094a74d7775df5e4f5840a7c0
k00b <k00b@stacker.news>
2026-02-16 21:13:14 -0600

    adjust reward subsidy

--- worker/earn.js
+++ worker/earn.js
@@ -148,7 +148,7 @@ export async function earn ({ name }) {
   }
 }
 
-const DAILY_STIMULUS_SATS = 10_000
+const DAILY_STIMULUS_SATS = 3_125
 export async function earnRefill ({ models, lnd }) {
   return await pay('DONATE',
     { sats: DAILY_STIMULUS_SATS },

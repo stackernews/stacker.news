5908e7902cc4d79230957c482d28cf6a524d0ca8
k00b <k00b@stacker.news>
2026-03-07 20:35:11 -0600

    better url dedupe checks

--- components/discussion-form.js
+++ components/discussion-form.js
@@ -1,9 +1,5 @@
 import { Form, Input, SNInput } from '@/components/form'
-import { gql, useLazyQuery } from '@apollo/client'
 import AdvPostForm from './adv-post-form'
-import { ITEM_FIELDS } from '@/fragments/items'
-import AccordianItem from './accordian-item'
-import Item from './item'
 import { discussionSchema } from '@/lib/validate'
 import { MAX_TITLE_LENGTH } from '@/lib/constants'
 import { ItemButtonBar } from './post'
@@ -22,18 +18,6 @@ export function DiscussionForm ({
     storageKeyPrefix: 'discussion',
     schemaFn: discussionSchema
   })
-
-  const [getRelated, { data: relatedData }] = useLazyQuery(gql`
-    ${ITEM_FIELDS}
-    query related($title: String!) {
-      related(title: $title, minMatch: "75%", limit: 3) {
-        items {
-          ...ItemFields
-        }
-      }
-    }`)
-
-  const related = relatedData?.related?.items || []
   return (
     <Form
       initial={initial}
@@ -48,13 +32,6 @@ export function DiscussionForm ({
         required
         autoFocus
         clear
-        onChange={async (formik, e) => {
-          if (e.target.value) {
-            getRelated({
-              variables: { title: e.target.value }
-            })
-          }
-        }}
         maxLength={MAX_TITLE_LENGTH}
       />
       <SNInput
@@ -66,19 +43,6 @@ export function DiscussionForm ({
       />
       <AdvPostForm storageKeyPrefix={storageKeyPrefix} item={item} />
       <ItemButtonBar itemId={item?.id} />
-      {!item &&
-        <div className={`mt-3 ${related.length > 0 ? '' : 'invisible'}`}>
-          <AccordianItem
-            header={<div style={{ fontWeight: 'bold', fontSize: '92%' }}>similar</div>}
-            body={
-              <div>
-                {related.map((item, i) => (
-                  <Item item={item} key={item.id} />
-                ))}
-              </div>
-              }
-          />
-        </div>}
     </Form>
   )
 }

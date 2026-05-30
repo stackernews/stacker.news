511083a23694bffabb249f82732aa7ec9280c44a
k00b <k00b@stacker.news>
2026-05-30 16:48:37 -0500

    Revert "refactor: extract NavContent for better readability, with price bar for custom domains"
    This reverts commit f87da97a7a195737c493040b77d9f132f64a8a17.

--- components/nav/mobile/top-bar.js
+++ components/nav/mobile/top-bar.js
@@ -5,34 +5,19 @@ import { useMe } from '@/components/me'
 import { useCommentsNavigatorContext, CommentsNavigator } from '@/components/use-comments-navigator'
 import { useBranding } from '@/components/territory-branding'
 
-function NavContent ({ sub, path, pathname }) {
+export default function TopBar ({ prefix, sub, path, pathname, topNavKey, dropNavKey }) {
   const { me } = useMe()
   const { navigator, commentCount } = useCommentsNavigatorContext()
   const branding = useBranding()
 
-  if (!hasNavSelect({ path, pathname })) {
-    return (
-      <>
-        <NavPrice className='flex-shrink-1' />
-        <CommentsNavigator navigator={navigator} commentCount={commentCount} className='px-2' />
-        {me ? <NavWalletSummary /> : <SignUpButton width='fit-content' />}
-      </>
-    )
+  // on mobile, we don't show the top bar if it contains a nav select on custom domains
+  // on mobile, the top bar with nav select is only shown on ~/, ~/new/*, ~/top/*
+  // as a consquence, those three paths will not have a back button on custom domains
+  // but, they continue to have a back button on the sticky bar when scrolling down
+  if (branding && hasNavSelect({ path, pathname })) {
+    return null
   }
 
-  if (branding) {
-    return (
-      <>
-        <NavPrice className='flex-shrink-1' />
-        {me && <NavWalletSummary />}
-      </>
-    )
-  }
-
-  return <NavSelect sub={sub} className='w-100' />
-}
-
-export default function TopBar ({ prefix, sub, path, pathname, topNavKey, dropNavKey }) {
   return (
     <Navbar>
       <Nav
@@ -40,7 +25,14 @@ export default function TopBar ({ prefix, sub, path, pathname, topNavKey, dropNa
         activeKey={topNavKey}
       >
         <Back className='d-flex d-md-none' />
-        <NavContent sub={sub} path={path} pathname={pathname} />
+        {hasNavSelect({ path, pathname })
+          ? <NavSelect sub={sub} className='w-100' />
+          : (
+            <>
+              <NavPrice className='flex-shrink-1' />
+              <CommentsNavigator navigator={navigator} commentCount={commentCount} className='px-2' />
+              {me ? <NavWalletSummary /> : <SignUpButton width='fit-content' />}
+            </>)}
       </Nav>
     </Navbar>
   )

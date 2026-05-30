68ce380c03027e764d4ad1ccac50daea8cd8a8b3
k00b <k00b@stacker.news>
2026-05-30 15:58:51 -0500

    remove top bar with nav select on mobile custom domains

--- components/nav/mobile/top-bar.js
+++ components/nav/mobile/top-bar.js
@@ -3,10 +3,20 @@ import styles from '../../header.module.css'
 import { Back, NavPrice, NavSelect, NavWalletSummary, SignUpButton, hasNavSelect } from '../common'
 import { useMe } from '@/components/me'
 import { useCommentsNavigatorContext, CommentsNavigator } from '@/components/use-comments-navigator'
+import { useBranding } from '@/components/territory-branding'
 
 export default function TopBar ({ prefix, sub, path, pathname, topNavKey, dropNavKey }) {
   const { me } = useMe()
   const { navigator, commentCount } = useCommentsNavigatorContext()
+  const branding = useBranding()
+
+  // on mobile, we don't show the top bar if it contains a nav select on custom domains
+  // on mobile, the top bar with nav select is only shown on ~/, ~/new/*, ~/top/*
+  // as a consquence, those three paths will not have a back button on custom domains
+  // but, they continue to have a back button on the sticky bar when scrolling down
+  if (branding && hasNavSelect({ path, pathname })) {
+    return null
+  }
 
   return (
     <Navbar>
@@ -15,13 +25,13 @@ export default function TopBar ({ prefix, sub, path, pathname, topNavKey, dropNa
         activeKey={topNavKey}
       >
         <Back className='d-flex d-md-none' />
-        {!branding && hasNavSelect({ path, pathname })
+        {hasNavSelect({ path, pathname })
           ? <NavSelect sub={sub} className='w-100' />
           : (
             <>
               <NavPrice className='flex-shrink-1' />
               <CommentsNavigator navigator={navigator} commentCount={commentCount} className='px-2' />
-              {me ? <NavWalletSummary /> : branding ? null : <SignUpButton width='fit-content' />}
+              {me ? <NavWalletSummary /> : <SignUpButton width='fit-content' />}
             </>)}
       </Nav>
     </Navbar>

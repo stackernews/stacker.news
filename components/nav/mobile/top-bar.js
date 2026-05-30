f87da97a7a195737c493040b77d9f132f64a8a17
soxa <6390896+Soxasora@users.noreply.github.com>
2026-05-30 23:21:57 +0200

    refactor: extract NavContent for better readability, with price bar for custom domains

--- components/nav/mobile/top-bar.js
+++ components/nav/mobile/top-bar.js
@@ -5,19 +5,34 @@ import { useMe } from '@/components/me'
 import { useCommentsNavigatorContext, CommentsNavigator } from '@/components/use-comments-navigator'
 import { useBranding } from '@/components/territory-branding'
 
-export default function TopBar ({ prefix, sub, path, pathname, topNavKey, dropNavKey }) {
+function NavContent ({ sub, path, pathname }) {
   const { me } = useMe()
   const { navigator, commentCount } = useCommentsNavigatorContext()
   const branding = useBranding()
 
-  // on mobile, we don't show the top bar if it contains a nav select on custom domains
-  // on mobile, the top bar with nav select is only shown on ~/, ~/new/*, ~/top/*
-  // as a consquence, those three paths will not have a back button on custom domains
-  // but, they continue to have a back button on the sticky bar when scrolling down
-  if (branding && hasNavSelect({ path, pathname })) {
-    return null
+  if (!hasNavSelect({ path, pathname })) {
+    return (
+      <>
+        <NavPrice className='flex-shrink-1' />
+        <CommentsNavigator navigator={navigator} commentCount={commentCount} className='px-2' />
+        {me ? <NavWalletSummary /> : <SignUpButton width='fit-content' />}
+      </>
+    )
   }
 
+  if (branding) {
+    return (
+      <>
+        <NavPrice className='flex-shrink-1' />
+        {me && <NavWalletSummary />}
+      </>
+    )
+  }
+
+  return <NavSelect sub={sub} className='w-100' />
+}
+
+export default function TopBar ({ prefix, sub, path, pathname, topNavKey, dropNavKey }) {
   return (
     <Navbar>
       <Nav
@@ -25,14 +40,7 @@ export default function TopBar ({ prefix, sub, path, pathname, topNavKey, dropNa
         activeKey={topNavKey}
       >
         <Back className='d-flex d-md-none' />
-        {hasNavSelect({ path, pathname })
-          ? <NavSelect sub={sub} className='w-100' />
-          : (
-            <>
-              <NavPrice className='flex-shrink-1' />
-              <CommentsNavigator navigator={navigator} commentCount={commentCount} className='px-2' />
-              {me ? <NavWalletSummary /> : <SignUpButton width='fit-content' />}
-            </>)}
+        <NavContent sub={sub} path={path} pathname={pathname} />
       </Nav>
     </Navbar>
   )
